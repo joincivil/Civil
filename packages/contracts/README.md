15ec10a192e041363d539531046c345de5edf1a9
Olaf Tomalka <olaf.tomalka@gmail.com>
2017-12-21 20:18:19 +0100

    Add CircleCI integration
    * Added CircleCI integration
    * Fix CircleCi having wrong job name
    * Fix identation in .yml files
    * Added CircleCI badge
    * Fix badge not having proper API token
    * Fix badge's markdown

--- packages/contracts/README.md
+++ packages/contracts/README.md
@@ -1,2 +1,2 @@
 # contracts
-Smart Contracts needed to make the ecosystem work
+[![CircleCI](https://circleci.com/gh/joincivil/contracts.svg?style=svg&circle-token=408d7a840b95720dc2e188dfd7060b307dda7cc0)](https://circleci.com/gh/joincivil/contracts)
--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -3,4 +3,13 @@ module.exports = {
   // to customize your Truffle configuration!
   migrations_directory: "transpiled/migrations",
   test_directory: "transpiled/test",
+  mocha: {
+    reporter: 'mocha-multi-reporters',
+    reporterOptions: {
+      reporterEnabled: "mocha-junit-reporter, spec",
+      mochaJunitReporterReporterOptions: {
+        mochaFile: './transpiled/junit/junit.xml'
+      }
+    }
+  }
 };
