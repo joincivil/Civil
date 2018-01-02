ea12367b12dc26fc5118ba98f3a9943b11b9deda
Olaf Tomalka <olaf.tomalka@gmail.com>
2018-01-02 19:17:00 +0100

    Merge pull request #3 from joincivil/feature/strip-artifacts
    Added a script to strip debug data from artifacts

---
+++ packages/contracts/scripts/strip-artifacts.ts
@@ -0,0 +1,59 @@
+import * as fs from "fs";
+import * as glob from "glob";
+import * as mkdirp from "mkdirp";
+import * as path from "path";
+
+/*
+ * Artifacts in the newest version of Truffle have a lot of data we don't need nor want in our library
+ * We're striping it to not leak and also have easier time managing JSONs
+ * The only things we're leaving are:
+ * - contractName
+ * - abi
+ * - bytecode (for deploying)
+ * - deployedBytecode (for source code confirming)
+ * - networks
+ */
+
+const JSON_WHITESPACE = 4;
+
+function strip(filePath: string, outPath: string) {
+  const data = JSON.parse(fs.readFileSync(filePath) as any);
+  /* tslint:disable object-literal-sort-keys */
+  const filtered = {
+    contractName: data.contractName,
+    bytecode: data.bytecode,
+    deployedBytecode: data.deployedBytecode,
+    abi: data.abi,
+    networks: data.networks,
+  };
+  /* tslint:enable object-literal-sort-keys */
+  fs.writeFileSync(outPath, JSON.stringify(filtered, null, JSON_WHITESPACE));
+  console.log("Written: " + outPath);
+}
+
+if (process.argv.length !== 4) {
+  console.error("Usage: " + process.argv[0] + " json_files_glob out_dir");
+  process.exit();
+}
+
+const FILES_GLOB = process.argv[2];
+const OUT_DIR = process.argv[3];
+
+// Ensure out directory exists
+mkdirp.sync(OUT_DIR);
+
+glob(FILES_GLOB, (err, files) => {
+  if (err) {
+    console.error("Failed to parse glob", err);
+    return process.exit(1);
+  }
+
+  files.forEach((filePath) => {
+    const filename = path.basename(filePath);
+    const outPath = path.posix.format({
+      base: filename,
+      dir: OUT_DIR,
+    } as any);
+    strip(filePath, outPath);
+  });
+});
--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  migrations_directory: "transpiled/migrations",
-  test_directory: "transpiled/test",
+  migrations_directory: "build/migrations",
+  test_directory: "build/test",
   mocha: {
     reporter: 'mocha-multi-reporters',
     reporterOptions: {
       reporterEnabled: "mocha-junit-reporter, spec",
       mochaJunitReporterReporterOptions: {
-        mochaFile: './transpiled/junit/junit.xml'
+        mochaFile: './build/junit/junit.xml'
       }
     }
   }
