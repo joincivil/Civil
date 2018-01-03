7e3ca307d51f64d64f64467df5244c3be16dfbbc
Olaf Tomalka <olaf@tomalka.me>
2018-01-03 16:23:54 +0100

    Revert "Added ganache cli to the project"
    This reverts commit 2086a9113d56516815b09a6ff288e2eed9412056.
    No need for ganache cli and breaks CI

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -1,13 +1,6 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-    }
-  },
   migrations_directory: "build/migrations",
   test_directory: "build/test",
   mocha: {
