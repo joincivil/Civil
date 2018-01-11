a29453916ce685b8bd14d5e41134091a36ed2a95
Olaf Tomalka <olaf@tomalka.me>
2018-01-11 20:16:59 +0100

    Update to latest abi-gen
    Newest abi-gen has the PRs we needed to make it working.
    Moved to upstream version and so deleted our included abi-gen

--- packages/civil.ts/ext/abi-gen/lib/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ParamKind;
-(function (ParamKind) {
-    ParamKind["Input"] = "input";
-    ParamKind["Output"] = "output";
-})(ParamKind = exports.ParamKind || (exports.ParamKind = {}));
-//# sourceMappingURL=types.js.map
