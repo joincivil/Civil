115cf7646e4556599de62af464119e021e864117
Olaf Tomalka <olaf.tomalka@gmail.com>
2018-01-13 11:38:45 +0100

    Merge pull request #6 from joincivil/nickreynolds/spike_tcr
    TCR Spike

---
+++ packages/contracts/contracts/RegistryWithAppellate.sol
@@ -0,0 +1,62 @@
+pragma solidity 0.4.18;
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+contract RegistryWithAppellate is Ownable {
+// TODO: Extend ACL & Registry (Mike Goldin)
+// https://github.com/skmgoldin/tcr/blob/master/contracts/Registry.sol
+
+  event AppealRequested(address indexed listing);
+  event AppealGranted(address indexed listing);
+  event AppealDenied(address indexed listing);
+
+  struct Listing {  
+    address owner; // owner of listing 
+    uint timestamp;
+    bool whitelisted;
+  }
+
+  // Maps listingHashes to associated listing data
+  mapping(address => Listing) public listings;
+
+  /// @dev returns true if listing is whitelisted
+  function isWhitelisted(address listing) public view returns (bool whitelisted) {
+    return listings[listing].whitelisted;
+  }
+
+  function listingOwner(address listing) public view returns (address) {
+    return listings[listing].owner;
+  }
+
+  function timestampOfAppeal(address listing) public view returns (uint) {
+    return listings[listing].timestamp;
+  }
+
+  function submitAppeal(address listing) public returns (address) {
+    require(listings[listing].owner == 0x0);
+    require(!listings[listing].whitelisted); 
+
+    listings[listing] = Listing(
+      msg.sender,
+      now,
+      false
+    );
+    AppealRequested(listing);
+    return listing;
+  }
+
+  function grantAppeal(address listing) public onlyOwner {
+    require(listings[listing].owner != 0x0);
+    require(!listings[listing].whitelisted);
+    listings[listing].whitelisted = true;
+    // TODO: Fire Registry event as well
+    AppealGranted(listing);
+  }
+
+  function denyAppeal(address listing) public onlyOwner {
+    require(listings[listing].owner != 0x0);
+    require(!listings[listing].whitelisted);
+    delete listings[listing];
+    AppealDenied(listing);
+  }
+}
---
+++ packages/contracts/test/registryWithAppellate.ts
@@ -0,0 +1,114 @@
+import * as chai from "chai";
+import { events, REVERTED } from "../utils/constants";
+import ChaiConfig from "./utils/chaiconfig";
+import { findEvent, idFromEvent, is0x0Address, timestampFromTx } from "./utils/contractutils";
+
+const RegistryWithAppellate = artifacts.require("RegistryWithAppellate");
+
+ChaiConfig();
+const expect = chai.expect;
+
+// TODO: Revisit and add many more tests after RegistryWtihAppellate extends real TCR
+contract("RegistryWithAppellate", (accounts: string[]) => {
+  const defaultAccount = accounts[0];
+
+  const EXISTING_LISTING_ADDRESS = accounts[2];
+  const NO_LISTING_ADDRESS = accounts[3];
+
+  let registry: any;
+  let id: any;
+
+  beforeEach(async () => {
+    registry = await RegistryWithAppellate.new();
+    const tx = await registry.submitAppeal(EXISTING_LISTING_ADDRESS, { from: accounts[1] });
+    id = idFromEvent(tx);
+  });
+
+  describe("owner", () => {
+    it("returns 0x0 on non-existent listing", async () => {
+      const is0x0 = is0x0Address(await registry.listingOwner(NO_LISTING_ADDRESS));
+      expect(is0x0).to.be.true();
+    });
+
+    it("returns correct owner for existing listing", async () => {
+      expect(await registry.listingOwner(EXISTING_LISTING_ADDRESS)).to.be.equal(accounts[1]);
+    });
+
+    it("returns 0x0 on denied listing", async () => {
+      await expect(registry.denyAppeal(EXISTING_LISTING_ADDRESS)).to.eventually.be.fulfilled();
+      const is0x0 = is0x0Address(await registry.listingOwner(EXISTING_LISTING_ADDRESS));
+      expect(is0x0).to.be.true();
+    });
+  });
+
+  describe("appeal", () => {
+    it("cannot be granted by non-owner of registry", async () => {
+      await expect(registry.grantAppeal(EXISTING_LISTING_ADDRESS, { from : accounts[1] }))
+      .to.eventually.be.rejectedWith(REVERTED);
+    });
+
+    it("cannot be denied by non-owner of registry", async () => {
+      await expect(registry.denyAppeal(EXISTING_LISTING_ADDRESS, { from : accounts[1] }))
+      .to.eventually.be.rejectedWith(REVERTED);
+    });
+
+    it("fails on granting appeal of non-existent listing", async () => {
+      await expect(registry.grantAppeal(NO_LISTING_ADDRESS)).to.eventually.be.rejectedWith(REVERTED);
+    });
+
+    it("fails on denying appeal of non-existent listing", async () => {
+      await expect(registry.denyAppeal(NO_LISTING_ADDRESS)).to.eventually.be.rejectedWith(REVERTED);
+    });
+  });
+
+  describe("submit appeal", () => {
+    it("fails on listing already present", async () => {
+      await expect(registry.submitAppeal(EXISTING_LISTING_ADDRESS, { from: accounts[1] }))
+      .to.eventually.be.rejectedWith(REVERTED);
+    });
+
+    it("succeeds on listing not already present", async () => {
+      await expect(registry.submitAppeal(NO_LISTING_ADDRESS, { from: accounts[1] })).to.eventually.be.fulfilled();
+    });
+  });
+
+  describe("grant appeal", () => {
+    it("listing is not whitelisted before appeal granted", async () => {
+      expect(await registry.isWhitelisted(EXISTING_LISTING_ADDRESS)).to.be.false();
+    });
+
+    it("succeeds on existent non-whitelisted listing", async () => {
+      await expect(registry.grantAppeal(EXISTING_LISTING_ADDRESS)).to.eventually.be.fulfilled();
+    });
+
+    it("listing is whitelisted after appeal granted", async () => {
+      await expect(registry.grantAppeal(EXISTING_LISTING_ADDRESS));
+      expect(await registry.isWhitelisted(EXISTING_LISTING_ADDRESS)).to.be.true();
+    });
+
+    it("fails on already whitelisted listing", async () => {
+      await registry.grantAppeal(EXISTING_LISTING_ADDRESS);
+      await expect(registry.grantAppeal(EXISTING_LISTING_ADDRESS)).to.eventually.be.rejectedWith(REVERTED);
+    });
+  });
+
+  describe("deny appeal", () => {
+    it("succeeds on existent non-whitelisted listing", async () => {
+      await expect(registry.denyAppeal(EXISTING_LISTING_ADDRESS)).to.eventually.be.fulfilled();
+    });
+
+    it("is not whitelisted after appeal denied", async () => {
+      expect(await registry.isWhitelisted(EXISTING_LISTING_ADDRESS)).to.be.false();
+    });
+
+    it("fails on already whitelisted listing", async () => {
+      await registry.grantAppeal(EXISTING_LISTING_ADDRESS);
+      await expect(registry.denyAppeal(EXISTING_LISTING_ADDRESS)).to.eventually.be.rejectedWith(REVERTED);
+    });
+
+    it("fails on already denied listing", async () => {
+      await registry.denyAppeal(EXISTING_LISTING_ADDRESS);
+      await expect(registry.denyAppeal(EXISTING_LISTING_ADDRESS)).to.eventually.be.rejectedWith(REVERTED);
+    });
+  });
+});
