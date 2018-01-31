4a916862ca7e0c67a8c3f1c028a7976ee3e1280d
Olaf Tomalka <olaf@tomalka.me>
2018-01-31 11:54:06 +0100

    Added basic article loading code

--- packages/debug-ui/src/code/entry.ts
+++ packages/debug-ui/src/code/entry.ts
@@ -1,7 +1,21 @@
 import { Civil } from "@joincivil/core";
+import * as marked from "marked";
+
+import { getParameters } from "./parameters";
 
 // Metamask is injected after full load
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
     const civil = new Civil();
     console.log(civil);
+
+    const params = await getParameters();
+    console.log(params);
+    const newsroom = await civil.newsroomAtUntrusted(params.newsroomAddress);
+    console.log(newsroom);
+    const article = await newsroom.loadArticle(params.articleId);
+    console.log(article);
+
+    const parsedContent = marked(article.content);
+    document.getElementById('content')!.innerHTML = parsedContent;
+    console.log(parsedContent);
 });
---
+++ packages/debug-ui/src/code/parameters.ts
@@ -0,0 +1,29 @@
+/**
+ * Checks for the # part of the url and if the parametrs of newsroom and the article are there,
+ * use them.
+ * Otherwise open a html form and request that data from the user
+ */
+
+export interface ArticleParams {
+    newsroomAddress: string;
+    articleId: number;
+}
+export async function getParameters(): Promise<ArticleParams> {
+    return new Promise<ArticleParams>((resolve, reject) => {
+        const content = document.getElementById("content")!;
+        content.classList.add("hidden");
+        const inputDiv = document.getElementById("param-input")!;
+        inputDiv.classList.remove("hidden");
+        const submitButton = document.getElementById("param-submit")!;
+        submitButton.onclick = (event) => {
+            const newsroomAddress = (document.getElementById("newsroom-address") as any).value;
+            const articleId = +((document.getElementById("article-id") as any).value);
+            inputDiv.classList.add("hidden");
+            content.classList.remove("hidden");
+            resolve({
+                newsroomAddress,
+                articleId
+            });
+        }
+    });
+}
