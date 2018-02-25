7294d1aa635e1f9902775c7e26eaf9d323579b25
Olaf Tomalka <olaf@tomalka.me>
2018-02-26 00:17:51 +0100

    Fix smart-contract tests not passing

--- packages/contracts/test/registry/challeneCanBeResolved.ts
+++
@@ -1,8 +0,0 @@
-/* TODO: implement
-contract("Registry", () => {
-  describe("Function: challengeCanBeResolved", () => {
-    it("should return true for a poll that has ended");
-    it("should return false if the poll either doesnt exist, or its still in contention");
-  });
-});
-*/
