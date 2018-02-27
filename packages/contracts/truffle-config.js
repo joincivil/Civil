511fb24a1adeaf7711aca5434cc3e02317886bb1
Olaf Tomalka <olaf@tomalka.me>
2018-02-27 14:54:30 +0100

    Removed mocha-multi-reporters from contracts
    We're not using test reporting from CircleCI and mocha-multi-reporters
    destroys error reporting from tests, makign it extremely hard to debug

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -3,15 +3,6 @@ module.exports = {
   // to customize your Truffle configuration!
   migrations_directory: "build/migrations",
   test_directory: "build/test",
-  mocha: {
-    reporter: 'mocha-multi-reporters',
-    reporterOptions: {
-      reporterEnabled: "mocha-junit-reporter, spec",
-      mochaJunitReporterReporterOptions: {
-        mochaFile: './build/junit/junit.xml'
-      }
-    }
-  },
   networks: {
     develop: {
       host: "localhost",
