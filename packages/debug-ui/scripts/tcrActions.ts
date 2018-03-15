f8594b01fd3dec6b1f7b9a7207b746a1b56e0761
Nick Reynolds <nick@joincivil.com>
2018-03-14 22:35:23 -0400

    check token allowance before applying in debug-ui

--- packages/debug-ui/scripts/tcrActions.ts
+++ packages/debug-ui/scripts/tcrActions.ts
@@ -9,7 +9,15 @@ export async function apply(address: EthAddress, _civil?: Civil): Promise<void> 
   const tcrAddress = civil.getDeployedTCRAddressForCurrentNetwork();
   const tcr = await civil.ownedAddressTCRWithAppealsAtUntrusted(tcrAddress);
 
-  const { awaitReceipt } = await tcr.apply(address, new BN(1000, 10), "test");
+  const eip20Address = await tcr.getTokenAddress();
+  const eip = await civil.eip20AtUntrusted(eip20Address);
+
+  const approvedTokensForSpender = await eip.getApprovedTokensForSpender(tcrAddress);
+  if (approvedTokensForSpender.toNumber() < 1000) {
+    await eip.approveSpender(tcrAddress, new BN(1000 - approvedTokensForSpender.toNumber()));
+  }
+
+  const { awaitReceipt } = await tcr.apply(address, new BN(1000), "test");
   const applyReceipt = await awaitReceipt;
   console.log("Applied to TCR");
 }
