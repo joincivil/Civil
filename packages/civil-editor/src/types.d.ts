58217cc08e36df68d76862b60c76975c3c4b3f4e
walfly <walker.d.flynn@gmail.com>
2018-03-21 17:59:22 -0400

    delete civil-editor

--- packages/civil-editor/src/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'slate';
-declare module 'slate-schema-violations';
-declare module 'slate-react';
-declare module 'slate-edit-list';
-declare module 'slate-drop-or-paste-images';
-declare module 'material-ui-icons';
-declare module '@storybook/addon-storyshots';
