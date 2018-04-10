991e61b0f4850559f99e0ee79386e7991373ea4d
walfly <walker.d.flynn@gmail.com>
2018-04-10 00:11:19 -0400

    pass value as json to civil editor

--- packages/editor/src/defaultValue.ts
+++ packages/editor/src/defaultValue.ts
@@ -11,7 +11,6 @@ export const valueJson = {
             object: "text",
             leaves: [
               {
-                object: "leaf",
                 text: "A paragraph",
               },
             ],
