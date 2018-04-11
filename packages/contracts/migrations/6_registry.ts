5969bbcc4c520ab58df6ea98289b0a46e67846d2
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-04-11 15:10:45 -0700

    fixing migrations

--- packages/contracts/migrations/6_registry.ts
+++ packages/contracts/migrations/6_registry.ts
@@ -7,30 +7,23 @@ const Token = artifacts.require("EIP20.sol");
 const DLL = artifacts.require("dll/DLL.sol");
 const AttributeStore = artifacts.require("attrstore/AttributeStore.sol");
 
-const OwnedAddressTCRWithAppeals = artifacts.require("tcr/OwnedAddressTCRWithAppeals.sol");
+const CivilTCR = artifacts.require("tcr/CivilTCR.sol");
 const Parameterizer = artifacts.require("tcr/Parameterizer.sol");
 const PLCRVoting = artifacts.require("tcr/PLCRVoting.sol");
 
 module.exports = (deployer: any, network: string, accounts: string[]) => {
   deployer.then(async () => {
-    await deployer.link(DLL, OwnedAddressTCRWithAppeals);
-    await deployer.link(AttributeStore, OwnedAddressTCRWithAppeals);
+    await deployer.link(DLL, CivilTCR);
+    await deployer.link(AttributeStore, CivilTCR);
 
     let tokenAddress = config.TokenAddress;
 
     if (network !== MAIN_NETWORK) {
       tokenAddress = Token.address;
     }
-    await deployer.deploy(
-      OwnedAddressTCRWithAppeals,
-      tokenAddress,
-      PLCRVoting.address,
-      Parameterizer.address,
-      accounts[0],
-      accounts[0],
-    );
+    await deployer.deploy(CivilTCR, tokenAddress, PLCRVoting.address, Parameterizer.address, accounts[0], accounts[0]);
     if (inTesting(network)) {
-      await approveEverything(accounts, Token.at(tokenAddress), OwnedAddressTCRWithAppeals.address);
+      await approveEverything(accounts, Token.at(tokenAddress), CivilTCR.address);
     }
   });
 };
