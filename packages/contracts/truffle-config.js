86b1967888df110d54da4426529fcf0592189dd8
Olaf Tomalka <olaf@tomalka.me>
2018-04-24 17:27:00 +0200

    Added solidity coverage

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -20,5 +20,12 @@ module.exports = {
       network_id: 4,
       gasPrice: "10000000000",
     },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555, // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01, // <-- Use this low gas price
+    },
   },
 };
