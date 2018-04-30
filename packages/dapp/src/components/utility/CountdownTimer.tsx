7419156aaaf19d2b6457369b0091c8b0cc74efe7
Jon Ferrer <jon@joincivil.com>
2018-04-30 15:15:19 -0400

    Updates for the `CountdownTimer` view
    - Stop the timer when the expiry has elapsed
    - Display the time in a more human friendly format (days, hours,
    seconds)

--- packages/dapp/src/components/utility/CountdownTimer.tsx
+++ packages/dapp/src/components/utility/CountdownTimer.tsx
@@ -6,6 +6,7 @@ export interface CountdownTimerProps {
 
 export interface CountdownTimerState {
   secondsRemaining: number;
+  timer?: number;
 }
 
 class CountdownTimer extends React.Component<CountdownTimerProps, CountdownTimerState> {
@@ -25,13 +26,40 @@ class CountdownTimer extends React.Component<CountdownTimerProps, CountdownTimer
   }
 
   private renderApplicationPhase(): JSX.Element {
-    return <>{this.state.secondsRemaining} seconds</>;
+    return <>{this.getReadableTimeRemaining()}</>;
+  }
+
+  private getReadableTimeRemaining = (): string => {
+    const out = [];
+    let period: [number, string];
+    const periods: Array<[number, string]> = [
+      [86400, "day"],
+      [3600, "hour"],
+      [60, "minute"],
+      [1, "second"]
+    ];
+    let secs: number = this.state.secondsRemaining;
+
+    while (periods.length) {
+      period = periods.shift()!;
+      const periodUnits = Math.floor(secs / period[0]);
+      if (periodUnits > 0) {
+        out.push(periodUnits.toString(), period[1] + (periodUnits !== 1 ? "s" : ""));
+        secs = secs % period[0];
+      }
+    }
+
+    return out.join(" ");
   }
 
   // TODO(nickreynolds): move this all into redux
   private initCountdown = async () => {
-    this.updateTimeRemaining();
-    setInterval(this.updateTimeRemaining, 1000);
+    const timeRemaining = this.updateTimeRemaining();
+    if (timeRemaining > 0) {
+      this.setState({ timer: window.setInterval(this.updateTimeRemaining, 1000) });
+    } else {
+      window.clearInterval(this.state.timer);
+    }
   };
 
   private updateTimeRemaining = () => {
@@ -39,6 +67,7 @@ class CountdownTimer extends React.Component<CountdownTimerProps, CountdownTimer
     const currentTime = parseInt((Date.now() / 1000).toString(), 10); // convert from milliseconds
     const secondsRemaining = expiry - currentTime;
     this.setState({ secondsRemaining });
+    return secondsRemaining;
   };
 }
 
