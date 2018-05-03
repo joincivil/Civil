95965ef915a3a107f009ad708e0817491f3adfc8
Unknown <nicholas.s.reynolds@gmail.com>
2018-05-02 22:49:38 -0700

    switching voting result logic around
    voting.isPassed is now associated with a successful challenge (listing/application to be removed) or a successful appeal challenge (granted appeal to be overturned) to reduce confusion
    
    fixing unit tests
    
    fix linting

--- packages/contracts/test/utils/contractutils.ts
+++ packages/contracts/test/utils/contractutils.ts
@@ -84,6 +84,66 @@ export async function challengeReparamAndGetPollID(
   return receipt.logs[0].args.pollID;
 }
 
+export async function simpleSuccessfulChallenge(
+  registry: any,
+  listing: string,
+  challenger: string,
+  voter: string,
+): Promise<void> {
+  const votingAddress = await registry.voting();
+  const voting = PLCRVoting.at(votingAddress);
+  const pollID = await challengeAndGetPollID(listing, challenger, registry);
+  await commitVote(voting, pollID, "1", "100", "123", voter);
+  await advanceEvmTime(paramConfig.commitStageLength + 1);
+  await voting.revealVote(pollID, "1", "123", { from: voter });
+  await advanceEvmTime(paramConfig.revealStageLength + 1);
+}
+
+export async function simpleUnsuccessfulChallenge(
+  registry: any,
+  listing: string,
+  challenger: string,
+  voter: string,
+): Promise<void> {
+  const votingAddress = await registry.voting();
+  const voting = PLCRVoting.at(votingAddress);
+  const pollID = await challengeAndGetPollID(listing, challenger, registry);
+  await commitVote(voting, pollID, "0", "100", "420", voter);
+  await advanceEvmTime(paramConfig.commitStageLength + 1);
+  await voting.revealVote(pollID, "0", "420", { from: voter });
+  await advanceEvmTime(paramConfig.revealStageLength + 1);
+}
+
+export async function simpleSuccessfulAppealChallenge(
+  registry: any,
+  listing: string,
+  challenger: string,
+  voter: string,
+): Promise<void> {
+  const votingAddress = await registry.voting();
+  const voting = PLCRVoting.at(votingAddress);
+  const pollID = await challengeAppealAndGetPollID(listing, challenger, registry);
+  await commitVote(voting, pollID, "1", "100", "123", voter);
+  await advanceEvmTime(paramConfig.appealChallengeCommitStageLength + 1);
+  await voting.revealVote(pollID, "1", "123", { from: voter });
+  await advanceEvmTime(paramConfig.appealChallengeRevealStageLength + 1);
+}
+
+export async function simpleUnsuccessfulAppealChallenge(
+  registry: any,
+  listing: string,
+  challenger: string,
+  voter: string,
+): Promise<void> {
+  const votingAddress = await registry.voting();
+  const voting = PLCRVoting.at(votingAddress);
+  const pollID = await challengeAppealAndGetPollID(listing, challenger, registry);
+  await commitVote(voting, pollID, "0", "100", "420", voter);
+  await advanceEvmTime(paramConfig.appealChallengeCommitStageLength + 1);
+  await voting.revealVote(pollID, "0", "420", { from: voter });
+  await advanceEvmTime(paramConfig.appealChallengeRevealStageLength + 1);
+}
+
 export async function addToWhitelist(
   listingAddress: string,
   deposit: BigNumber,
