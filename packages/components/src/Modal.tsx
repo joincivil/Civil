76a12cb3e756b5dd58b3afcee18c51a790200b18
walfly <walker.d.flynn@gmail.com>
2018-05-21 16:56:17 -0400

    add modal to patterns

--- packages/components/src/Modal.tsx
+++ packages/components/src/Modal.tsx
@@ -8,19 +8,31 @@ const ModalOuter = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+<<<<<<< HEAD
   background-color: rgba(255, 255, 255, 0.7);
+=======
+  background-color: rgba(255,255,255,0.7);
+>>>>>>> add modal to patterns
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const ModalInner = styled.div`
+<<<<<<< HEAD
   box-shadow: 0px 0px 20px 5px rgba(0, 0, 0, 0.6);
+=======
+  box-shadow: 0px 0px 20px 5px rgba(0, 0, 0, .6);
+>>>>>>> add modal to patterns
   max-width: 460px;
   padding: 35px;
   background: #fff;
 `;
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> add modal to patterns
 export class Modal extends React.Component {
   public bucket: HTMLDivElement = document.createElement("div");
 
@@ -34,6 +46,7 @@ export class Modal extends React.Component {
 
   public render(): React.ReactPortal {
     return ReactDOM.createPortal(
+<<<<<<< HEAD
       <ModalOuter>
         <ModalInner>{this.props.children}</ModalInner>
       </ModalOuter>,
@@ -41,3 +54,10 @@ export class Modal extends React.Component {
     );
   }
 }
+=======
+      <ModalOuter><ModalInner>{this.props.children}</ModalInner></ModalOuter>,
+      this.bucket,
+    );
+  };
+};
+>>>>>>> add modal to patterns
