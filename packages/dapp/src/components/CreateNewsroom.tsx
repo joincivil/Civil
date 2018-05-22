4be981fdb85614185b7e59c3d26c12e324102cb5
Toby Fox <tobyfox@gmail.com>
2018-05-21 21:38:00 -0700

    Add option to create multisig newsroom (WIP)
    Most newsroom management works on multisig, but not yet TCR stuff

--- packages/dapp/src/components/CreateNewsroom.tsx
+++ packages/dapp/src/components/CreateNewsroom.tsx
@@ -6,6 +6,7 @@ import { PageView, ViewModule, ViewModuleHeader } from "./utility/ViewModules";
 
 export interface CreateNewsroomState {
   name: string;
+  multisig: boolean;
   error: string;
 }
 export interface CreateNewsroomProps {
@@ -18,35 +19,45 @@ class CreateNewsroom extends React.Component<CreateNewsroomProps, CreateNewsroom
     super(props);
     this.state = {
       name: "",
+      multisig: false,
       error: "",
     };
   }
 
   public render(): JSX.Element {
-    console.log("this.props.history: " + this.props.history);
+    console.log("this.props.history:", this.props.history);
     return (
       <PageView>
         <ViewModule>
           <ViewModuleHeader>Create Newsroom</ViewModuleHeader>
           {this.state.error}
-          <input onChange={this.onChange} />
+          <input name="name" onChange={this.onChange} />
           <TransactionButton
             transactions={[{ transaction: this.createNewsroom, postTransaction: this.onNewsroomCreated }]}
           >
             Deploy Newsroom
           </TransactionButton>
+          <br />
+          <input type="checkbox" name="multisig" checked={this.state.multisig} onChange={this.onChange} /> multisig
         </ViewModule>
       </PageView>
     );
   }
 
   private onChange = (e: any) => {
-    this.setState({ name: e.target.value });
+    const target = e.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
   };
 
   private createNewsroom = async (): Promise<TwoStepEthTransaction<any>> => {
     const civil = getCivil();
-    return civil.newsroomDeployNonMultisigTrusted(this.state.name);
+    if (this.state.multisig) {
+      return civil.newsroomDeployTrusted(this.state.name);
+    } else {
+      return civil.newsroomDeployNonMultisigTrusted(this.state.name);
+    }
   };
 
   private onNewsroomCreated = (result: any) => {
