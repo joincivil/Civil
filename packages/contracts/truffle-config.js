d1ae186e1db9d7653ca6a3dea19d943890ab06be
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-05-23 22:19:53 -0700

    running migrations

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -18,7 +18,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: 4,
-      gasPrice: "50000000000",
+      gasPrice: "20000000000",
     },
     coverage: {
       host: "localhost",
