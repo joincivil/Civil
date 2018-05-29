15c58f54273d1cf7fb588ee2bba6c6265344a9ab
Jon Ferrer <jon@joincivil.com>
2018-05-29 18:47:34 -0400

    Updates for `TransactionButton` progress modal
    - Implement Progress Modal for a `DetailedTransaction` button in
    stories.
    - Add `ModalHeading` component
    - Fix `CIVIL_TEAL` color hex value in style constants

--- packages/components/src/DetailTransactionButton.stories.tsx
+++ packages/components/src/DetailTransactionButton.stories.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 import styled from "styled-components";
 import { DetailTransactionButton } from "./DetailTransactionButton";
 import { Modal } from "./Modal";
+import { ModalContent, ModalHeading } from "./ModalContent";
+import { LoadingIndicator } from "./LoadingIndicator";
 import { Civil, TwoStepEthTransaction } from "@joincivil/core";
 
 let civil: Civil | undefined;
@@ -46,9 +48,11 @@ storiesOf("DetailTransactionButton", module)
   })
   .add("detail transaction button with visible progress modal", () => {
     const progressModal = (
-      <Modal>
-        <h3>Hello</h3>
-        <p>lorem ipsum whatever you know what i mean. It gets pretty long because thats more useful</p>
+      <Modal textAlign="center">
+        <LoadingIndicator height={100} />
+        <ModalHeading>Your transaction is in progress.</ModalHeading>
+        <ModalContent>This can take 1-3 minutes. Please don't close the tab.</ModalContent>
+        <ModalContent>How about taking a little breather and standing for a bit? Maybe even stretching?</ModalContent>
       </Modal>
     );
 
