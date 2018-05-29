1b994eebf0bbe58413b493aa1bf2530ea986dcae
Jon Ferrer <jon@joincivil.com>
2018-05-29 16:14:28 -0400

    Add support to display a 'Progress Modal' that renders when the user clicks a Transaction Button and disappears when the transactions are complete

--- packages/components/src/Modal.tsx
+++ packages/components/src/Modal.tsx
@@ -2,7 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import styled from "styled-components";
 
-const ModalOuter = styled.div`
+interface ToggleDisplayEl {
+  visible: boolean;
+}
+
+const ModalOuter = styled<ToggleDisplayEl, "div">("div")`
   position: absolute;
   top: 0;
   bottom: 0;
@@ -10,6 +14,7 @@ const ModalOuter = styled.div`
   right: 0;
   background-color: rgba(255, 255, 255, 0.7);
   display: flex;
+  display: ${props => (props.visible ? "flex" : "none")};
   justify-content: center;
   align-items: center;
 `;
@@ -22,8 +27,28 @@ const ModalInner = styled.div`
   background: #fff;
 `;
 
-export class Modal extends React.Component {
+export interface ModalPropsAndState {
+  visible?: boolean;
+}
+
+export class Modal extends React.Component<ModalPropsAndState, ModalPropsAndState> {
+  private static getDerivedStateFromProps(props: ModalPropsAndState, state: ModalPropsAndState): any | null {
+    if (typeof props.visible !== "undefined") {
+      const visible = props.visible;
+      return { visible };
+    }
+    return null;
+  }
+
   public bucket: HTMLDivElement = document.createElement("div");
+
+  public constructor(props: any) {
+    super(props);
+
+    this.state = {
+      visible: this.props.visible || true,
+    };
+  }
 
   public componentDidMount(): void {
     document.body.appendChild(this.bucket);
@@ -35,7 +60,7 @@ export class Modal extends React.Component {
 
   public render(): React.ReactPortal {
     return ReactDOM.createPortal(
-      <ModalOuter>
+      <ModalOuter visible={this.props.visible!}>
         <ModalInner>{this.props.children}</ModalInner>
       </ModalOuter>,
       this.bucket,
