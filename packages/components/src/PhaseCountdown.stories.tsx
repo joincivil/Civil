2ce3197663025db967d5d55bce1cbe0912f5cd97
Jon Ferrer <jon@joincivil.com>
2018-06-13 16:15:06 -0400

    Use a different, but more straightforward, kludge to handle the Storyshot issue with the timer stuff.
    (Thx @walfly for talking me off the ledge)

--- packages/components/src/PhaseCountdown.stories.tsx
+++ packages/components/src/PhaseCountdown.stories.tsx
@@ -2,7 +2,6 @@ import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import styled from "styled-components";
 import { CountdownTimer } from "./PhaseCountdown";
-import { Button, buttonSizes } from "./Button";
 
 const StyledDiv = styled.div`
   display: flex;
@@ -11,52 +10,25 @@ const StyledDiv = styled.div`
   width: 400px;
 `;
 
-interface ContainerState {
-  initialized: boolean;
-}
-
-class Container extends React.Component<{}, ContainerState> {
-  constructor(props: any) {
-    super(props);
-    this.state = { initialized: false };
-  }
-
-  // TODO(jon): This is a total kludge for the fact that Storyshots doesn't like
-  // our dynamically generated dates for these timers, and Storyshots doesn't seem
-  // to support Jest Property Matchers (which would solve this problem)
-  // https://facebook.github.io/jest/docs/en/snapshot-testing.html#property-matchers
-  public render(): JSX.Element {
-    if (this.state.initialized) {
-      return <StyledDiv>{this.props.children}</StyledDiv>;
-    }
-    return (
-      <StyledDiv>
-        <Button onClick={this.initialize} size={buttonSizes.MEDIUM}>
-          Initialize Timers
-        </Button>
-      </StyledDiv>
-    );
-  }
-
-  private initialize = (event: any): void => {
-    this.setState({ initialized: true });
-  };
-}
-
+const Container: React.StatelessComponent = ({ children }) => <StyledDiv>{children}</StyledDiv>;
 const now = Date.now() / 1000;
 const eightDaysFromNow = now + 86400 * 8;
 
 storiesOf("Application Phase Countdown Timer", module).add("Timers", () => {
   return (
     <Container>
-      <h3>Base style</h3>
-      <CountdownTimer endTime={eightDaysFromNow} />
+      {process.env.NODE_ENV !== "test" && (
+        <>
+          <h3>Base style</h3>
+          <CountdownTimer endTime={eightDaysFromNow} />
 
-      <h3>Warning style</h3>
-      <CountdownTimer endTime={eightDaysFromNow} warn={true} />
+          <h3>Warning style</h3>
+          <CountdownTimer endTime={eightDaysFromNow} warn={true} />
 
-      <h3>Ended</h3>
-      <CountdownTimer endTime={now} />
+          <h3>Ended</h3>
+          <CountdownTimer endTime={now} />
+        </>
+      )}
     </Container>
   );
 });
