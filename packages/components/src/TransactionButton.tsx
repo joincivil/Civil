0ac7ca70840cee8d5acc0f573c4b50ada9071aed
Jon Ferrer <jon@joincivil.com>
2018-06-25 09:59:45 -0700

    Debug console.log statement

--- packages/components/src/TransactionButton.tsx
+++ packages/components/src/TransactionButton.tsx
@@ -209,6 +209,7 @@ export class TransactionButton extends React.Component<
     modalComponent: JSX.Element | undefined,
     modalContentComponents?: any,
   ): JSX.Element | undefined => {
+    console.log("get progress modal");
     const modalContentSource =
       (modalContentComponents && modalContentComponents[this.state.progressModalState!]) ||
       DEFAULT_MODAL_COMPONENTS[this.state.progressModalState!];
