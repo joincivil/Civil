3bbaeb4a8d01e2f624798737981c1fc4a5e1d36a
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-07-16 12:51:44 -0700

    updating .solcover.js

--- .solcover.js
+++ .solcover.js
@@ -5,6 +5,7 @@ module.exports = {
     "installed_contracts/DLL.sol",
     "installed_contracts/EIP20.sol",
     "installed_contracts/EIP20Interface.sol",
+    "installed_contracts/PLCRVoting.sol",
     "zeppelin-solidity/ECRecovery.sol",
     "zeppelin-solidity/Ownable.sol",
     "zeppelin-solidity/SafeMath.sol",
