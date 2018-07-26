757a9ff6248f467c660d269bc8baa86afa1a28cc
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-07-26 12:28:27 -0700

    fixing unit tests after changes to contracts

--- packages/contracts/test/utils/contractutils.ts
+++ packages/contracts/test/utils/contractutils.ts
@@ -87,9 +87,9 @@ export async function simpleSuccessfulChallenge(
   const votingAddress = await registry.voting();
   const voting = PLCRVoting.at(votingAddress);
   const pollID = await challengeAndGetPollID(listing, challenger, registry);
-  await commitVote(voting, pollID, "1", "100", "123", voter);
+  await commitVote(voting, pollID, "0", "100", "123", voter);
   await advanceEvmTime(paramConfig.commitStageLength + 1);
-  await voting.revealVote(pollID, "1", "123", { from: voter });
+  await voting.revealVote(pollID, "0", "123", { from: voter });
   await advanceEvmTime(paramConfig.revealStageLength + 1);
 }
 
@@ -102,9 +102,9 @@ export async function simpleUnsuccessfulChallenge(
   const votingAddress = await registry.voting();
   const voting = PLCRVoting.at(votingAddress);
   const pollID = await challengeAndGetPollID(listing, challenger, registry);
-  await commitVote(voting, pollID, "0", "100", "420", voter);
+  await commitVote(voting, pollID, "1", "100", "420", voter);
   await advanceEvmTime(paramConfig.commitStageLength + 1);
-  await voting.revealVote(pollID, "0", "420", { from: voter });
+  await voting.revealVote(pollID, "1", "420", { from: voter });
   await advanceEvmTime(paramConfig.revealStageLength + 1);
 }
 
@@ -117,9 +117,9 @@ export async function simpleSuccessfulAppealChallenge(
   const votingAddress = await registry.voting();
   const voting = PLCRVoting.at(votingAddress);
   const pollID = await challengeAppealAndGetPollID(listing, challenger, registry);
-  await commitVote(voting, pollID, "1", "100", "123", voter);
+  await commitVote(voting, pollID, "0", "100", "123", voter);
   await advanceEvmTime(paramConfig.appealChallengeCommitStageLength + 1);
-  await voting.revealVote(pollID, "1", "123", { from: voter });
+  await voting.revealVote(pollID, "0", "123", { from: voter });
   await advanceEvmTime(paramConfig.appealChallengeRevealStageLength + 1);
 }
 
@@ -132,9 +132,9 @@ export async function simpleUnsuccessfulAppealChallenge(
   const votingAddress = await registry.voting();
   const voting = PLCRVoting.at(votingAddress);
   const pollID = await challengeAppealAndGetPollID(listing, challenger, registry);
-  await commitVote(voting, pollID, "0", "100", "420", voter);
+  await commitVote(voting, pollID, "1", "100", "420", voter);
   await advanceEvmTime(paramConfig.appealChallengeCommitStageLength + 1);
-  await voting.revealVote(pollID, "0", "420", { from: voter });
+  await voting.revealVote(pollID, "1", "420", { from: voter });
   await advanceEvmTime(paramConfig.appealChallengeRevealStageLength + 1);
 }
 
