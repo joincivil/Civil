1b1d7a7c49044fdeab2519a40d903f43d5d4e87a
Jon Ferrer <jon@joincivil.com>
2018-07-26 17:38:01 -0400

    Add TCR Dashboard components: A collection of components for rendering user-related information regarding the TCR
    - Examples are challenges voted on, the user's balance, etc

---
+++ packages/components/src/TCRUserDashboard/DashboardActivity.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import styled from "styled-components";
+import { colors } from "../styleConstants";
+
+const StyledUserActivity = styled.div`
+  background-color: transparent;
+`;
+
+const StyledUserActivityHeader = styled.h3`
+  color: ${colors.basic.WHITE};
+  font-size: 18px;
+  font-weight: normal;
+  line-height: 21px;
+`;
+
+const StyledUserActivityContent = styled.h3`
+  background-color: ${colors.basic.WHITE};
+  border: 1px solid ${colors.accent.CIVIL_GRAY_4};
+  border-top: none;
+`;
+
+export const DashboardActivity: React.StatelessComponent = props => {
+  return (
+    <StyledUserActivity>
+      <StyledUserActivityHeader>My Tasks</StyledUserActivityHeader>
+      <StyledUserActivityContent>{props.children}</StyledUserActivityContent>
+    </StyledUserActivity>
+  );
+};
---
+++ packages/components/src/TCRUserDashboard/index.ts
@@ -0,0 +1,4 @@
+export * from "./Dashboard";
+export * from "./DashboardUserInfoSummary";
+export * from "./DashboardActivity";
+export * from "./DashboardActivityItem";
