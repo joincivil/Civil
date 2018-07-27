a570209aa5aa2992e9577ffc3659ac3572374a79
walfly <walker.d.flynn@gmail.com>
2018-07-27 14:40:30 -0400

    update how hover styling works for view transaction button

--- packages/components/src/ViewTransactionLink.tsx
+++ packages/components/src/ViewTransactionLink.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { TxHash } from "@joincivil/core";
-import styled, {withTheme} from "styled-components";
+import styled from "styled-components";
 import { colors, fonts } from "./styleConstants";
 import { NorthEastArrow } from "./icons";
 
@@ -8,11 +8,6 @@ export interface ViewTransactionLinkProps {
   txHash: TxHash;
   network: string;
   text?: string;
-  theme: LinkTheme;
-}
-
-export interface ViewTransactionLinkState {
-  hovered: boolean;
 }
 
 export interface LinkTheme {
@@ -24,7 +19,7 @@ const defaultProps = {
   theme: {
     linkColor: colors.accent.CIVIL_BLUE,
     linkColorHover: colors.accent.CIVIL_BLUE_FADED,
-    sanserifFont: fonts.SANS_SERIF,
+    sansSerifFont: fonts.SANS_SERIF,
   },
 };
 
@@ -32,28 +27,24 @@ const StyledLink = styled.a`
   text-decoration: none;
   font-family: ${props => props.theme.sanserifFont};
   color: ${props => props.theme.linkColor};
+  &:hover {
+    color: ${props => props.theme.linkColorHover};
+  }
+  & path {
+    fill: ${props => props.theme.linkColor};
+  }
+  &:hover path {
+    fill: ${props => props.theme.linkColorHover};
+  }
 `;
 
 StyledLink.defaultProps = defaultProps;
 
-class ViewTransactionLinkComponent extends React.Component<ViewTransactionLinkProps, ViewTransactionLinkState> {
-  constructor(props: ViewTransactionLinkProps) {
-    super(props);
-    this.state = {
-      hovered: false,
-    };
-  }
-  public render(): JSX.Element {
-    const baseUrl = this.props.network === "rinkeby" ? "https://rinkeby.etherscan.io/tx/" : "https://etherscan.io/tx/";
-    return (
-      <StyledLink onMouseOver={() => this.setState({hovered: true})} onMouseLeave={() => this.setState({hovered: false})} target="_blank" href={`${baseUrl}${this.props.txHash}`}>
-        {this.props.text || "view transaction"} <NorthEastArrow color={this.state.hovered ? this.props.theme.linkColorHover : this.props.theme.linkColor} />
-      </StyledLink>
-    );
-  }
-
-}
-
-export const ViewTransactionLink = withTheme(ViewTransactionLinkComponent);
-
-ViewTransactionLink.defaultProps = defaultProps;
+export const ViewTransactionLink = (props: ViewTransactionLinkProps) => {
+  const baseUrl = props.network === "rinkeby" ? "https://rinkeby.etherscan.io/tx/" : "https://etherscan.io/tx/";
+  return (
+    <StyledLink target="_blank" href={`${baseUrl}${props.txHash}`}>
+      {props.text || "view transaction"} <NorthEastArrow />
+    </StyledLink>
+  );
+};
--- packages/components/src/theme.ts
+++ packages/components/src/theme.ts
@@ -2,5 +2,5 @@ import { ButtonTheme } from "./Button";
 import { LinkTheme } from "./ViewTransactionLink";
 
 export interface Theme extends ButtonTheme, LinkTheme {
-  sanserifFont: string;
+  sansSerifFont: string;
 }
