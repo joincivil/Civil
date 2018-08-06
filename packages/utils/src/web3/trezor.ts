1a43cd86ba50993822489a4a09afcddb2ff069d2
Jorge Lopez <jl5659@gmail.com>
2018-08-06 10:59:45 -0400

    Adding trezor-connect lib

--- packages/utils/src/web3/trezor.ts
+++ packages/utils/src/web3/trezor.ts
@@ -1,10 +1,8 @@
 import HookedWalletSubprovider = require("web3-provider-engine/subproviders/hooked-wallet");
 import * as TrezorConnect from "trezor-connect";
 import * as Web3 from "web3";
-import {
-  addHexPrefix
-} from "ethereumjs-util";
-import * as EthereumTx from "ethereumjs-tx";
+import { addHexPrefix } from "ethereumjs-util";
+import EthereumTx = require("ethereumjs-tx");
 
 function toTrezorHex(hex: string): string | undefined {
   let trezorHex;
@@ -25,8 +23,7 @@ function toTrezorHex(hex: string): string | undefined {
 }
 // Inspired by github.com/gnosis/MultiSigWallet/blob/6f1f8fc37fd53a7c87548997cc603755b5d2cca1/dapp/services/Web3Service.js#L349-L408
 
-export function getTrezorWeb3(networkId: string = "4", path: string = "m/44'/60'/0'/0/0" ): Web3 {
-
+export function getTrezorWeb3(networkId: string = "4", path: string = "m/44'/60'/0'/0/0"): Web3 {
   return new HookedWalletSubprovider({
     getAccounts(cb: (err: any, res?: any) => any): void {
       TrezorConnect.ethereumGetAddress(path, (response: any) => {
@@ -41,36 +38,33 @@ export function getTrezorWeb3(networkId: string = "4", path: string = "m/44'/60'
       cb(null, true);
     },
     signTransaction(txData: any, cb: (err: any, res?: any) => any): void {
-
-        TrezorConnect.ethereumSignTx(
-          path, // address path - either array or string, see example
-          toTrezorHex(txData.nonce), // nonce - hexadecimal string
-          toTrezorHex(txData.gasPrice), // gas price - hexadecimal string
-          toTrezorHex(txData.gas), // gas limit - hexadecimal string
-          txData.to ? toTrezorHex(txData.to) : null, // address
-          toTrezorHex(txData.value), // value in wei, hexadecimal string
-          txData.data ? toTrezorHex(txData.data) : null, // data, hexadecimal string OR null for no data
-          parseInt(networkId, 16), // chain id for EIP-155 - is only used in fw 1.4.2 and newer, older will ignore it
-          (response: any) => {
-            if (response.success) {
-              txData.value = txData.value || "0x00";
-              txData.data =  addHexPrefix(txData.data);
-              txData.gasPrice = parseInt(txData.gasPrice, 16);
-              txData.nonce = parseInt(txData.nonce, 16);
-              txData.gasLimit = txData.gas;
-              txData.v = response.v;
-              txData.s = "0x" + response.s;
-              txData.r = "0x" + response.r;
-              // Sign transaction
-              const tx = new EthereumTx(txData);
-              cb(null, "0x" + tx.serialize().toString("hex"));
-            } else {
-              cb(response.error);
-            }
-          },
-        );
-      });
+      TrezorConnect.ethereumSignTx(
+        path, // address path - either array or string, see example
+        toTrezorHex(txData.nonce), // nonce - hexadecimal string
+        toTrezorHex(txData.gasPrice), // gas price - hexadecimal string
+        toTrezorHex(txData.gas), // gas limit - hexadecimal string
+        txData.to ? toTrezorHex(txData.to) : null, // address
+        toTrezorHex(txData.value), // value in wei, hexadecimal string
+        txData.data ? toTrezorHex(txData.data) : null, // data, hexadecimal string OR null for no data
+        parseInt(networkId, 16), // chain id for EIP-155 - is only used in fw 1.4.2 and newer, older will ignore it
+        (response: any) => {
+          if (response.success) {
+            txData.value = txData.value || "0x00";
+            txData.data = addHexPrefix(txData.data);
+            txData.gasPrice = parseInt(txData.gasPrice, 16);
+            txData.nonce = parseInt(txData.nonce, 16);
+            txData.gasLimit = txData.gas;
+            txData.v = response.v;
+            txData.s = "0x" + response.s;
+            txData.r = "0x" + response.r;
+            // Sign transaction
+            const tx = new EthereumTx(txData);
+            cb(null, "0x" + tx.serialize().toString("hex"));
+          } else {
+            cb(response.error);
+          }
+        },
+      );
     },
   });
-
 }
