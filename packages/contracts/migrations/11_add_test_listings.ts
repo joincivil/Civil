e0658c6ac04a7fe0fac10b9b5d77cd337d025255
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-08-21 09:31:51 -0400

    undo config changes

--- packages/contracts/migrations/11_add_test_listings.ts
+++ packages/contracts/migrations/11_add_test_listings.ts
@@ -1,5 +1,3 @@
-import { config } from "./utils";
-
 module.exports = (deployer: any, network: string, accounts: string[]) => {
   const Newsroom = artifacts.require("Newsroom");
   const CivilTCR = artifacts.require("CivilTCR");
@@ -10,18 +8,18 @@ module.exports = (deployer: any, network: string, accounts: string[]) => {
       const civilTCR = await CivilTCR.deployed();
       const token = await Token.deployed();
 
-      await token.approve(civilTCR.address, config.paramDefaults.minDeposit * 10, { from: accounts[5] });
-      await token.approve(civilTCR.address, config.paramDefaults.minDeposit * 10, { from: accounts[6] });
+      await token.approve(civilTCR.address, 1000, { from: accounts[5] });
+      await token.approve(civilTCR.address, 1000, { from: accounts[6] });
 
       const newsroom1 = await Newsroom.new("Fake News 1000", "http://fakecharter.uri", web3.sha3(), {
         from: accounts[5],
       });
-      await civilTCR.apply(newsroom1.address, config.paramDefaults.minDeposit, "", { from: accounts[5] });
+      await civilTCR.apply(newsroom1.address, 100, "", { from: accounts[5] });
 
       const newsroom2 = await Newsroom.new("The Ultra News 2000", "https://fakecharter.com", web3.sha3(), {
         from: accounts[5],
       });
-      await civilTCR.apply(newsroom2.address, config.paramDefaults.minDeposit, "", { from: accounts[5] });
+      await civilTCR.apply(newsroom2.address, 100, "", { from: accounts[5] });
       await civilTCR.challenge(newsroom2.address, "", { from: accounts[5] });
     }
   });
