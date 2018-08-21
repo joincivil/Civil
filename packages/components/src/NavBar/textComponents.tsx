bc270a4aedb41c0d3da5d8c8cd67004d752d8201
sruddy <sarah.ruddy@gmail.com>
2018-08-21 18:34:46 -0400

    text component cleanup

--- packages/components/src/NavBar/textComponents.tsx
+++ packages/components/src/NavBar/textComponents.tsx
@@ -1,67 +1,35 @@
 import * as React from "react";
 
 // Nav Links
-export const NavLinkRegistryText: React.SFC = props => {
-  return <>Registry</>;
-};
+export const NavLinkRegistryText: React.SFC = props => <>Registry</>;
 
-export const NavLinkParameterizerText: React.SFC = props => {
-  return <>Parameterizer</>;
-};
+export const NavLinkParameterizerText: React.SFC = props => <>Parameterizer</>;
 
-export const NavLinkCreateNewsroomText: React.SFC = props => {
-  return <>Create Newsroom</>;
-};
+export const NavLinkCreateNewsroomText: React.SFC = props => <>Create Newsroom</>;
 
-export const NavLinkConstitutionText: React.SFC = props => {
-  return <>Constitution</>;
-};
+export const NavLinkConstitutionText: React.SFC = props => <>Constitution</>;
 
-export const NavLinkAboutText: React.SFC = props => {
-  return <>About</>;
-};
+export const NavLinkAboutText: React.SFC = props => <>About</>;
 
-export const NavLinkLaunchNewsroomText: React.SFC = props => {
-  return <>How to launch a newsroom</>;
-};
+export const NavLinkLaunchNewsroomText: React.SFC = props => <>How to launch a newsroom</>;
 
-export const NavLinkWhitePaperText: React.SFC = props => {
-  return <>White Paper</>;
-};
+export const NavLinkWhitePaperText: React.SFC = props => <>White Paper</>;
 
-export const NavLinkDashboardText: React.SFC = props => {
-  return <>My Activity</>;
-};
+export const NavLinkDashboardText: React.SFC = props => <>My Activity</>;
 
 // Nav Drawer
-export const NavDrawerBalanceText: React.SFC = props => {
-  return <>Balance</>;
-};
+export const NavDrawerBalanceText: React.SFC = props => <>Balance</>;
 
-export const NavDrawerTotalBalanceText: React.SFC = props => {
-  return <>Total Balance</>;
-};
+export const NavDrawerTotalBalanceText: React.SFC = props => <>Total Balance</>;
 
-export const NavDrawerVotingBalanceText: React.SFC = props => {
-  return <>Voting Balance</>;
-};
+export const NavDrawerVotingBalanceText: React.SFC = props => <>Voting Balance</>;
 
-export const NavDrawerBuyCvlBtnText: React.SFC = props => {
-  return <>Buy CVL Tokens</>;
-};
+export const NavDrawerBuyCvlBtnText: React.SFC = props => <>Buy CVL Tokens</>;
 
-export const NavDrawerCopyBtnText: React.SFC = props => {
-  return <>Copy</>;
-};
+export const NavDrawerCopyBtnText: React.SFC = props => <>Copy</>;
 
-export const NavDrawerDashboardText: React.SFC = props => {
-  return <>Dashboard</>;
-};
+export const NavDrawerDashboardText: React.SFC = props => <>Dashboard</>;
 
-export const NavDrawerSubmittedChallengesText: React.SFC = props => {
-  return <>Submitted Challenges</>;
-};
+export const NavDrawerSubmittedChallengesText: React.SFC = props => <>Submitted Challenges</>;
 
-export const NavDrawerVotedChallengesText: React.SFC = props => {
-  return <>Challenges Voted On</>;
-};
+export const NavDrawerVotedChallengesText: React.SFC = props => <>Challenges Voted On</>;
