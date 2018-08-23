911db3aec2f7578956fb217c16744c4bda24c2df
Toby Fox <tobyfox@gmail.com>
2018-08-22 17:31:02 -0700

    Fix monospace font constant

--- packages/components/src/styleConstants.ts
+++ packages/components/src/styleConstants.ts
@@ -28,5 +28,5 @@ export const colors = {
 export const fonts = {
   SERIF: `"Spectral", serif`,
   SANS_SERIF: `"Libre Franklin", sans-serif`,
-  MONOSPACE: `font-family: “SF Mono”, “Segoe UI Mono”, “Roboto Mono”, “Ubuntu Mono”, Menlo, Courier, monospace;`,
+  MONOSPACE: `"SF Mono", "Segoe UI Mono", "Roboto Mono", "Ubuntu Mono", Menlo, Courier, monospace;`,
 };
