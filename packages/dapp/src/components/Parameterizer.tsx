a6e2edaee3a92f3ad11afff78e13d005cdb25bb6
Jon Ferrer <jon@joincivil.com>
2018-09-05 18:11:41 -0400

    Apply table view to Parameterizer UI

--- packages/dapp/src/components/Parameterizer.tsx
+++ packages/dapp/src/components/Parameterizer.tsx
@@ -1,18 +1,92 @@
 import * as React from "react";
 import styled from "styled-components";
-import { State } from "../reducers";
 import { connect, DispatchProp } from "react-redux";
 import { BigNumber } from "bignumber.js";
-import { PageView, ViewModule, ViewModuleHeader } from "./utility/ViewModules";
+import {
+  Button,
+  buttonSizes,
+  colors,
+  fonts,
+  Table,
+  Tr,
+  Td,
+  Th,
+  Tabs,
+  Tab,
+  StyledDashboardSubTab,
+  MinDepositLabelText,
+  ParamMinDepositLabelText,
+  ApplicationStageLenLabelText,
+  ParamApplicationStageLenLabelText,
+  CommitStageLenLabelText,
+  ParamCommitStageLenLabelText,
+  RevealStageLenLabelText,
+  ParamRevealStageLenLabelText,
+  DispensationPctLabelText,
+  ParamDispensationPctLabelText,
+  VoteQuorumLabelText,
+  ParamVoteQuorumLabelText,
+  ParamProcessByLabelText,
+  ChallengeAppealLenLabelText,
+  ChallengeAppealCommitStageLenLabelText,
+  ChallengeAppealRevealStageLenLabelText,
+  RequestAppealLenLabelText,
+  JudgeAppealLenLabelText,
+  AppealFeeLabelText,
+  AppealVotePercentageLabelText,
+} from "@joincivil/components";
+import { State } from "../reducers";
 import { ProposeReparameterization } from "./parameterizer/proposeReparameterization";
 import { GovernmentReparameterization } from "./parameterizer/GovernmentReparameterization";
 import Proposals from "./parameterizer/Proposals";
 import { getFormattedTokenBalance, getReadableDuration, Parameters, GovernmentParameters } from "@joincivil/utils";
+import ListingDiscourse from "./listing/ListingDiscourse";
 import { getCivil } from "../helpers/civilInstance";
 
-const StyledSpan = styled.span`
-  font-weight: bold;
-  margin: 0 10px 0 0;
+const GridRow = styled.div`
+  display: flex;
+  margin: 40px auto 0;
+  padding: 0 0 20px;
+  width: 1200px;
+
+  & ~ & {
+    margin-top: 0;
+  }
+`;
+
+const GridRowStatic = styled.div`
+  margin: 0 auto;
+  width: 1200px;
+`;
+
+const StyledTabContainer = styled.div`
+  padding: 30px 0 50px;
+`;
+
+const StyledTitle = styled.div`
+  color: ${colors.primary.CIVIL_GRAY_1};
+  font-family: ${fonts.SERIF};
+  font-size: 48px;
+  font-weight: 200;
+  margin: 0;
+  line-height: 40px;
+  letter-spacing: -0.19px;
+  width: 350px;
+`;
+
+const StyledDescriptionP = styled.p`
+  color: ${colors.primary.CIVIL_GRAY_1};
+  font-size: 18px;
+  line-height: 33px;
+  letter-spacing: -0.12px;
+  width: 730px;
+`;
+
+const StyledP = styled.p`
+  color: ${colors.primary.CIVIL_GRAY_1}
+  font-size: 16px;
+  line-height: 20px;
+  letter-spacing: -0.11px;
 `;
 
 export interface ParameterizerProps {
@@ -47,136 +121,217 @@ export interface ParameterizerPageProps {
 }
 
 class Parameterizer extends React.Component<ParameterizerPageProps & DispatchProp<any>> {
-  constructor(props: any) {
-    super(props);
-  }
-
-  // TODO(sruddy): Nguyet is working on designs for this so I'll update with html/css later
   public render(): JSX.Element {
     const civil = getCivil();
 
     return (
-      <PageView>
-        <ViewModule>
-          <ViewModuleHeader>Current Parameters</ViewModuleHeader>
+      <>
+        <GridRow>
+          <StyledTitle>Civil Registry Parameters</StyledTitle>
+          <StyledDescriptionP>
+            The Civil Registry runs on a set of rules called "parameters". These parameters control almost every
+            function of the Registry's governance, from how much it costs to challenge a newsroom to how long the voting
+            process will take. The Civil community has the power to propose new values for these parameters and new
+            rules as the Civil platform evolves.
+          </StyledDescriptionP>
+        </GridRow>
+        <GridRow>
+          <Tabs TabComponent={StyledDashboardSubTab}>
+            <Tab title="Current Parameters">
+              <StyledTabContainer>
+                <StyledP>
+                  All Civil token holders may propose a change to the current Registry values. This process involves
+                  three phases: proposal, challenge, and vote.
+                </StyledP>
+                <StyledP>
+                  Proposing new values and challenging a proposal{" "}
+                  <b>
+                    requires a deposit of{" "}
+                    {this.props.parameters[Parameters.pMinDeposit] &&
+                      getFormattedTokenBalance(
+                        civil.toBigNumber(this.props.parameters[Parameters.pMinDeposit].toString()),
+                      )}
+                  </b>.
+                </StyledP>
 
-          <div>
-            <StyledSpan>minDeposit:</StyledSpan>{" "}
-            {this.props.parameters.minDeposit &&
-              getFormattedTokenBalance(civil.toBigNumber(this.props.parameters.minDeposit.toString()))}
-          </div>
-          <div>
-            <StyledSpan>pMinDeposit:</StyledSpan>{" "}
-            {this.props.parameters.pMinDeposit &&
-              getFormattedTokenBalance(civil.toBigNumber(this.props.parameters.pMinDeposit.toString()))}
-          </div>
-          <div>
-            <StyledSpan>applyStageLen:</StyledSpan>{" "}
-            {this.props.parameters.applyStageLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.applyStageLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>pApplyStageLen:</StyledSpan>{" "}
-            {this.props.parameters.pApplyStageLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.pApplyStageLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>commitStageLen:</StyledSpan>{" "}
-            {this.props.parameters.commitStageLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.commitStageLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>pCommitStageLen:</StyledSpan>{" "}
-            {this.props.parameters.pCommitStageLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.pCommitStageLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>revealStageLen:</StyledSpan>{" "}
-            {this.props.parameters.revealStageLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.revealStageLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>pRevealStageLen:</StyledSpan>{" "}
-            {this.props.parameters.pRevealStageLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.pRevealStageLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>dispensationPct:</StyledSpan>{" "}
-            {this.props.parameters.dispensationPct && this.props.parameters.dispensationPct.toString()}
-          </div>
-          <div>
-            <StyledSpan>pDispensationPct:</StyledSpan>{" "}
-            {this.props.parameters.pDispensationPct && this.props.parameters.pDispensationPct.toString()}
-          </div>
-          <div>
-            <StyledSpan>voteQuorum:</StyledSpan>{" "}
-            {this.props.parameters.voteQuorum && this.props.parameters.voteQuorum.toString()}
-          </div>
-          <div>
-            <StyledSpan>pVoteQuorum:</StyledSpan>{" "}
-            {this.props.parameters.pVoteQuorum && this.props.parameters.pVoteQuorum.toString()}
-          </div>
-          <div>
-            <StyledSpan>pProcessBy:</StyledSpan>{" "}
-            {this.props.parameters.pProcessBy &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.pProcessBy.toString()))}
-          </div>
-          <div>
-            <StyledSpan>challengeAppealLen:</StyledSpan>{" "}
-            {this.props.parameters.challengeAppealLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.challengeAppealLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>challengeAppealCommitLen:</StyledSpan>{" "}
-            {this.props.parameters.challengeAppealCommitLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.challengeAppealCommitLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>challengeAppealRevealLen:</StyledSpan>{" "}
-            {this.props.parameters.challengeAppealRevealLen &&
-              getReadableDuration(civil.toBigNumber(this.props.parameters.challengeAppealRevealLen.toString()))}
-          </div>
-          <br />
-          <ViewModuleHeader> Goverment Parameters </ViewModuleHeader>
-          <div>
-            <StyledSpan>requestAppealLen:</StyledSpan>{" "}
-            {this.props.govtParameters.requestAppealLen &&
-              getReadableDuration(civil.toBigNumber(this.props.govtParameters.requestAppealLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>judgeAppealLen:</StyledSpan>{" "}
-            {this.props.govtParameters.judgeAppealLen &&
-              getReadableDuration(civil.toBigNumber(this.props.govtParameters.judgeAppealLen.toString()))}
-          </div>
-          <div>
-            <StyledSpan>appealFee:</StyledSpan>{" "}
-            {this.props.govtParameters.appealFee &&
-              getFormattedTokenBalance(civil.toBigNumber(this.props.govtParameters.appealFee.toString()))}
-          </div>
-          <div>
-            <StyledSpan>appealVotePercentage:</StyledSpan>{" "}
-            {this.props.govtParameters.appealVotePercentage &&
-              this.props.govtParameters.appealVotePercentage.toString()}
-          </div>
-        </ViewModule>
+                <Table width="100%">
+                  <thead>
+                    <Tr>
+                      <Th>Current Parameter</Th>
+                      <Th>Current Value</Th>
+                      <Th colSpan={3} accent>
+                        Proposal for New Value
+                      </Th>
+                    </Tr>
+                  </thead>
+                  <tbody>
+                    {Object.keys(this.props.parameters).map(key => {
+                      if (!this.props.parameters[key]) {
+                        return <></>;
+                      }
+                      return (
+                        <Parameter
+                          key={key}
+                          parameterName={key}
+                          parameterDisplayName={this.getParameterDisplayName(key)}
+                          parameterValue={this.props.parameters[key]}
+                        />
+                      );
+                    })}
+                  </tbody>
+                </Table>
 
-        <ProposeReparameterization
-          paramKeys={Object.keys(this.props.parameters)}
-          pMinDeposit={this.props.parameters.pMinDeposit && this.props.parameters.pMinDeposit.toString()}
-        />
+                <ProposeReparameterization
+                  paramKeys={Object.keys(this.props.parameters)}
+                  pMinDeposit={this.props.parameters.pMinDeposit && this.props.parameters.pMinDeposit.toString()}
+                />
+              </StyledTabContainer>
+            </Tab>
 
-        <GovernmentReparameterization paramKeys={Object.keys(this.props.govtParameters)} />
+            <Tab title="Government Parameters">
+              <StyledTabContainer>
+                <Table width="100%">
+                  <thead>
+                    <Tr>
+                      <Th>Current Parameter</Th>
+                      <Th>Current Value</Th>
+                      <Th colSpan={3} accent>
+                        Proposal for New Value
+                      </Th>
+                    </Tr>
+                  </thead>
+                  <tbody>
+                    {Object.keys(this.props.govtParameters).map(key => {
+                      if (!this.props.govtParameters[key]) {
+                        return <></>;
+                      }
+                      return (
+                        <Parameter
+                          key={key}
+                          parameterName={key}
+                          parameterDisplayName={this.getParameterDisplayName(key)}
+                          parameterValue={this.props.govtParameters[key]}
+                        />
+                      );
+                    })}
+                  </tbody>
+                </Table>
+                <GovernmentReparameterization paramKeys={Object.keys(this.props.govtParameters)} />
+              </StyledTabContainer>
+            </Tab>
+          </Tabs>
+        </GridRow>
 
-        <Proposals />
-      </PageView>
+        <GridRow>
+          <Proposals />
+        </GridRow>
+
+        <GridRowStatic>
+          <ListingDiscourse />
+        </GridRowStatic>
+      </>
     );
   }
+
+  private getParameterDisplayName = (parameterName: string): JSX.Element => {
+    const parameterDisplayNameMap = {
+      [Parameters.minDeposit]: MinDepositLabelText,
+      [Parameters.pMinDeposit]: ParamMinDepositLabelText,
+      [GovernmentParameters.appealFee]: AppealFeeLabelText,
+
+      [Parameters.applyStageLen]: ApplicationStageLenLabelText,
+      [Parameters.pApplyStageLen]: ParamApplicationStageLenLabelText,
+      [Parameters.commitStageLen]: CommitStageLenLabelText,
+      [Parameters.pCommitStageLen]: ParamCommitStageLenLabelText,
+      [Parameters.revealStageLen]: RevealStageLenLabelText,
+      [Parameters.pRevealStageLen]: ParamRevealStageLenLabelText,
+      [Parameters.pProcessBy]: ParamProcessByLabelText,
+      [Parameters.challengeAppealLen]: ChallengeAppealLenLabelText,
+      [Parameters.challengeAppealCommitLen]: ChallengeAppealCommitStageLenLabelText,
+      [Parameters.challengeAppealRevealLen]: ChallengeAppealRevealStageLenLabelText,
+      [GovernmentParameters.requestAppealLen]: RequestAppealLenLabelText,
+      [GovernmentParameters.judgeAppealLen]: JudgeAppealLenLabelText,
+
+      [Parameters.dispensationPct]: DispensationPctLabelText,
+      [Parameters.pDispensationPct]: ParamDispensationPctLabelText,
+      [Parameters.voteQuorum]: VoteQuorumLabelText,
+      [Parameters.pVoteQuorum]: ParamVoteQuorumLabelText,
+      [GovernmentParameters.appealVotePercentage]: AppealVotePercentageLabelText,
+    };
+
+    let DisplayName: React.SFC = props => <></>;
+    if (parameterDisplayNameMap[parameterName]) {
+      DisplayName = parameterDisplayNameMap[parameterName];
+    }
+
+    return <DisplayName />;
+  };
+}
+
+interface ParameterProps {
+  parameterName: string;
+  parameterDisplayName: string | JSX.Element;
+  parameterValue: BigNumber;
+}
+
+class Parameter extends React.Component<ParameterProps> {
+  public render(): JSX.Element {
+    return (
+      <Tr>
+        <Td>{this.props.parameterDisplayName}</Td>
+        <Td>{this.getFormattedValue()}</Td>
+        <Td accent align="right" colSpan={3}>
+          <Button size={buttonSizes.SMALL}>Propose New Value</Button>
+        </Td>
+      </Tr>
+    );
+  }
+
+  private getFormattedValue = (): string => {
+    const civil = getCivil();
+
+    const amountParams: string[] = [Parameters.minDeposit, Parameters.pMinDeposit, GovernmentParameters.appealFee];
+
+    const durationParams: string[] = [
+      Parameters.applyStageLen,
+      Parameters.pApplyStageLen,
+      Parameters.commitStageLen,
+      Parameters.pCommitStageLen,
+      Parameters.revealStageLen,
+      Parameters.pRevealStageLen,
+      Parameters.pProcessBy,
+      Parameters.challengeAppealLen,
+      Parameters.challengeAppealCommitLen,
+      Parameters.challengeAppealRevealLen,
+      GovernmentParameters.requestAppealLen,
+      GovernmentParameters.judgeAppealLen,
+    ];
+
+    const percentParams: string[] = [
+      Parameters.dispensationPct,
+      Parameters.pDispensationPct,
+      Parameters.voteQuorum,
+      Parameters.pVoteQuorum,
+      GovernmentParameters.appealVotePercentage,
+    ];
+
+    let value = "";
+
+    if (amountParams.includes(this.props.parameterName)) {
+      value = getFormattedTokenBalance(civil.toBigNumber(this.props.parameterValue.toString()));
+    } else if (durationParams.includes(this.props.parameterName)) {
+      value = getReadableDuration(civil.toBigNumber(this.props.parameterValue.toString()));
+    } else if (percentParams.includes(this.props.parameterName)) {
+      value = `${this.props.parameterValue.toString()}%`;
+    }
+
+    return value;
+  };
 }
 
 const mapToStateToProps = (state: State): ParameterizerPageProps => {
   const parameters: ParameterizerProps = state.networkDependent.parameters as ParameterizerProps;
   const govtParameters: GovernmentParameterProps = state.networkDependent.govtParameters as GovernmentParameterProps;
-  console.log("govtParameters: ", govtParameters);
   return { parameters, govtParameters };
 };
 
