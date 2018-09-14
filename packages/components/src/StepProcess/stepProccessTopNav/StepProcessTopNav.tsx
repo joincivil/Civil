845e54af406591bc7c0e4f1c30cd3e79910dc80a
walfly <walker.d.flynn@gmail.com>
2018-09-14 00:03:12 -0400

    lint

--- packages/components/src/StepProcess/stepProccessTopNav/StepProcessTopNav.tsx
+++ packages/components/src/StepProcess/stepProccessTopNav/StepProcessTopNav.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import styled from "styled-components";
-import { colors } from "../../styleConstants";
 import { StepProps } from "./Step";
 
 export interface StepsProps {
