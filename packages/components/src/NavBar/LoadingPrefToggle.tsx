0e035eb52390bc1e9f6df5c9f04ab098f4c17b0e
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-10-17 14:03:21 -0400

    fix linting

--- packages/components/src/NavBar/LoadingPrefToggle.tsx
+++ packages/components/src/NavBar/LoadingPrefToggle.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import styled, { StyledComponentClass } from "styled-components";
 import { SlideCheckbox } from "../input/SlideCheckbox";
 
 export interface LoadingPrefToggleProps {
