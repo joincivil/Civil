4d501eaef8cae31227a8ec8cb0321ed7923cfe53
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-10-18 17:47:13 -0400

    cleanup

---
+++ packages/dapp/src/components/listinglist/WhitelistedListingItemContainer.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { connect, DispatchProp } from "react-redux";
+import { ListingSummaryComponent } from "@joincivil/components";
+import { getFormattedTokenBalance } from "@joincivil/utils";
+import { State } from "../../reducers";
+import { setupListingWhitelistedSubscription } from "../../actionCreators/listings";
+import { getListingPhaseState, makeGetLatestWhitelistedTimestamp } from "../../selectors";
+import { ListingListItemOwnProps, ListingListItemReduxProps } from "./ListingListItem";
+
+export interface WhitelistedCardReduxProps extends ListingListItemReduxProps {
+  whitelistedTimestamp?: number;
+}
+
+class WhitelistedListingItem extends React.Component<
+  ListingListItemOwnProps & WhitelistedCardReduxProps & DispatchProp<any>
+> {
+  public async componentDidMount(): Promise<void> {
+    this.props.dispatch!(await setupListingWhitelistedSubscription(this.props.listingAddress!));
+  }
+
+  public render(): JSX.Element {
+    const { listingAddress, listing, newsroom, listingPhaseState, charter } = this.props;
+    const listingData = listing!.data;
+    let description = "";
+    if (charter) {
+      try {
+        // TODO(jon): This is a temporary patch to handle the older charter format. It's needed while we're in transition to the newer schema and should be updated once the dapp is updated to properly handle the new charter
+        description = charter.desc;
+      } catch (ex) {
+        console.error("charter not formatted correctly");
+      }
+    }
+    const appExpiry = listingData.appExpiry && listingData.appExpiry.toNumber();
+    const pollData = listingData.challenge && listingData.challenge.poll;
+    const commitEndDate = pollData && pollData.commitEndDate.toNumber();
+    const revealEndDate = pollData && pollData.revealEndDate.toNumber();
+    const unstakedDeposit = listing && getFormattedTokenBalance(listing.data.unstakedDeposit);
+    const challengeStake = listingData.challenge && getFormattedTokenBalance(listingData.challenge.stake);
+
+    const newsroomData = newsroom!.data;
+    const listingDetailURL = `/listing/${listingAddress}`;
+    let whitelistedTimestamp = this.props.whitelistedTimestamp;
+    if (!whitelistedTimestamp && this.props.queryData) {
+      whitelistedTimestamp = new Date(this.props.queryData.approvalDate).getTime() / 1000;
+    }
+
+    const listingViewProps = {
+      ...newsroomData,
+      listingAddress,
+      description,
+      listingDetailURL,
+      ...listingPhaseState,
+      appExpiry,
+      commitEndDate,
+      revealEndDate,
+      unstakedDeposit,
+      challengeStake,
+      whitelistedTimestamp,
+    };
+
+    return <ListingSummaryComponent {...listingViewProps} />;
+  }
+}
+
+const makeMapStateToProps = () => {
+  const getLatestWhitelistedTimestamp = makeGetLatestWhitelistedTimestamp();
+
+  const mapStateToProps = (
+    state: State,
+    ownProps: ListingListItemOwnProps,
+  ): ListingListItemOwnProps & WhitelistedCardReduxProps => {
+    const { content } = state.networkDependent;
+    const whitelistedTimestamp = getLatestWhitelistedTimestamp(state, ownProps);
+    let charter;
+    if (ownProps.newsroom && ownProps.newsroom.data.charterHeader) {
+      charter = content.get(ownProps.newsroom.data.charterHeader);
+    }
+    return {
+      listingPhaseState: getListingPhaseState(ownProps.listing),
+      whitelistedTimestamp,
+      charter,
+      ...ownProps,
+    };
+  };
+
+  return mapStateToProps;
+};
+
+export default connect(makeMapStateToProps)(WhitelistedListingItem);
