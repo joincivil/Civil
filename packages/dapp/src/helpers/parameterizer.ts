9580cf691e5e977ea3a4df6e5837f8e9ab7cbdb6
Dan Kinsley <dan@joincivil.com>
2018-11-26 12:59:54 -0500

    disable propsIDsToProcess to fix CPU spike

--- packages/dapp/src/helpers/parameterizer.ts
+++ packages/dapp/src/helpers/parameterizer.ts
@@ -31,7 +31,8 @@ export async function initializeProposalsSubscriptions(dispatch: Dispatch<any>):
     parameterizer.propIDsInApplicationPhase(),
     parameterizer.propIDsInChallengeCommitPhase(),
     parameterizer.propIDsInChallengeRevealPhase(),
-    parameterizer.propIDsToProcess(),
+    // TODO(dankins): temporary change to fix CPU spiking until we can fix the root cause
+    // parameterizer.propIDsToProcess(),
     parameterizer.propIDsForResolvedChallenges(),
   ).subscribe(async (propID: string) => {
     const paramName = await parameterizer.getPropName(propID);
