31ad19f1f61913fa0df3562084dc372a0a4c7b9b
Toby Fox <tobyfox@gmail.com>
2018-11-26 08:07:01 -0500

    Ultra-basic roster

--- packages/dapp/src/components/listing/ListingCharter.tsx
+++ packages/dapp/src/components/listing/ListingCharter.tsx
@@ -24,9 +24,15 @@ class ListingCharter extends React.Component<ListingCharterProps> {
     }
 
     return <>
-      {Object.keys(charter.mission).map(key => <div key={key}>
+      {charter.mission && Object.keys(charter.mission).map(key => <div key={key}>
         <h3>{key[0].toUpperCase() + key.substr(1)}</h3>
         <p>{charter.mission[key]}</p>
+      </div>)}
+
+      <h2>Team</h2>
+      {charter.roster && charter.roster.map((rosterMember, i) => <div key={i}>
+        <h3>{rosterMember.name}</h3>
+        <p>{rosterMember.bio}</p>
       </div>)}
     </>;
   }
