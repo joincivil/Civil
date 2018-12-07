6e14724209ac91a888bcced484c18715ba504104
Jorge Lopez <jl5659@gmail.com>
2018-12-07 14:06:11 -0500

    Initial email util file

---
+++ packages/utils/src/email.ts
@@ -0,0 +1,25 @@
+import * as querystring from "querystring";
+
+// TODO(jorgelo): Drop this into a configuration.
+const SIGNUP_ENDPOINT = "https://us-central1-civil-media.cloudfunctions.net/addToSendgrid";
+
+export async function addToMailingList(email: string, listId: string): Promise<void> {
+  const query = {
+    email,
+    list_id: listId,
+  };
+  const url = SIGNUP_ENDPOINT + "?" + querystring.stringify(query);
+
+  console.log("Requesting: ", { url });
+  try {
+    const res = await fetch(url);
+
+    const body: any = await res.json();
+
+    console.log("Got body:", { body });
+  } catch (err) {
+    // TODO(jorgelo): Log error somewere on error?
+    console.error("Got error:", { err });
+    throw err;
+  }
+}
