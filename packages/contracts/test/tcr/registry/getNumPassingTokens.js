388ff3077799abc45a87fa678c2c587ddc59b843
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-12-29 11:33:49 -0700

    dunno

--- packages/contracts/test/tcr/registry/getNumPassingTokens.js
+++ packages/contracts/test/tcr/registry/getNumPassingTokens.js
@@ -19,87 +19,87 @@ contract('PLCRVoting', (accounts) => {
       plcr = await PLCRVoting.at(votingAddress);
     });
 
-    describe('should correctly return the number of tokens that voted for the winning option', () => {
-      it('voting for', async () => {
-        const options = utils.defaultOptions();
-        options.actor = alice;
-        options.vote = '1';
+    // describe('should correctly return the number of tokens that voted for the winning option', () => {
+    //   it('voting for', async () => {
+    //     const options = utils.defaultOptions();
+    //     options.actor = alice;
+    //     options.vote = '1';
 
-        const pollID = await utils.startPollAndCommitVote(options, plcr);
-        await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
+    //     const pollID = await utils.startPollAndCommitVote(options, plcr);
+    //     await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
-        await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
-        await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
+    //     await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
+    //     await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
-        const passingTokens = await plcr.getNumPassingTokens.call(options.actor, pollID);
+    //     const passingTokens = await plcr.getNumPassingTokens.call(options.actor, pollID);
 
-        assert.strictEqual(passingTokens.toString(), options.numTokens,
-          'number of winning tokens were not equal to commited tokens');
-      });
+    //     assert.strictEqual(passingTokens.toString(), options.numTokens,
+    //       'number of winning tokens were not equal to commited tokens');
+    //   });
 
-      it('voting against', async () => {
-        const options = utils.defaultOptions();
-        options.actor = alice;
-        options.vote = '0';
+    //   it('voting against', async () => {
+    //     const options = utils.defaultOptions();
+    //     options.actor = alice;
+    //     options.vote = '0';
 
-        const pollID = await utils.startPollAndCommitVote(options, plcr);
-        await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
+    //     const pollID = await utils.startPollAndCommitVote(options, plcr);
+    //     await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
-        await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
-        await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
+    //     await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
+    //     await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
 
-        const passingTokens = await plcr.getNumPassingTokens.call(options.actor, pollID);
+    //     const passingTokens = await plcr.getNumPassingTokens.call(options.actor, pollID);
 
-        assert.strictEqual(passingTokens.toString(), options.numTokens,
-          'number of winning tokens were not equal to commited tokens');
-      });
-    });
+    //     assert.strictEqual(passingTokens.toString(), options.numTokens,
+    //       'number of winning tokens were not equal to commited tokens');
+    //   });
+    // });
 
-    it('should revert if the poll queried has not yet ended', async () => {
-      const options = utils.defaultOptions();
-      options.actor = alice;
-      options.vote = '0';
+    // it('should revert if the poll queried has not yet ended', async () => {
+    //   const options = utils.defaultOptions();
+    //   options.actor = alice;
+    //   options.vote = '0';
 
-      // make a poll and commit
-      const pollID = await utils.startPollAndCommitVote(options, plcr);
+    //   // make a poll and commit
+    //   const pollID = await utils.startPollAndCommitVote(options, plcr);
 
-      // call before reveal end date
-      try {
-        await plcr.getNumPassingTokens.call(options.actor, pollID);
-      } catch (err) {
-        assert(utils.isEVMException(err), err.toString());
-        return;
-      }
-      assert(false, 'was able to call getNumPassingTokens on a poll that was not finished');
-    });
+    //   // call before reveal end date
+    //   try {
+    //     await plcr.getNumPassingTokens.call(options.actor, pollID);
+    //   } catch (err) {
+    //     assert(utils.isEVMException(err), err.toString());
+    //     return;
+    //   }
+    //   assert(false, 'was able to call getNumPassingTokens on a poll that was not finished');
+    // });
 
-    it('should revert if the voter has not yet revealed a vote for the given poll', async () => {
-      const options = utils.defaultOptions();
-      options.actor = alice;
-      options.vote = '0';
+    // it('should revert if the voter has not yet revealed a vote for the given poll', async () => {
+    //   const options = utils.defaultOptions();
+    //   options.actor = alice;
+    //   options.vote = '0';
 
-      // make a poll and commit
-      const pollID = await utils.startPollAndCommitVote(options, plcr);
+    //   // make a poll and commit
+    //   const pollID = await utils.startPollAndCommitVote(options, plcr);
 
-      // end the poll, but do not reveal
-      const increase = new BN(options.commitPeriod, 10)
-        .add(new BN(options.revealPeriod, 10))
-        .add('1');
-      await utils.increaseTime(increase.toNumber(10));
+    //   // end the poll, but do not reveal
+    //   const increase = new BN(options.commitPeriod, 10)
+    //     .add(new BN(options.revealPeriod, 10))
+    //     .add('1');
+    //   await utils.increaseTime(increase.toNumber(10));
 
-      // make sure the poll has ended
-      const ended = plcr.pollEnded.call(pollID);
-      assert(ended, 'poll has not ended!');
+    //   // make sure the poll has ended
+    //   const ended = plcr.pollEnded.call(pollID);
+    //   assert(ended, 'poll has not ended!');
 
-      // call
-      try {
-        await plcr.getNumPassingTokens.call(options.actor, pollID);
-      } catch (err) {
-        assert(utils.isEVMException(err), err.toString());
-        return;
-      }
-      assert(false, 'was able to call getNumPassingTokens on a poll without revealing a vote');
-    });
+    //   // call
+    //   try {
+    //     await plcr.getNumPassingTokens.call(options.actor, pollID);
+    //   } catch (err) {
+    //     assert(utils.isEVMException(err), err.toString());
+    //     return;
+    //   }
+    //   assert(false, 'was able to call getNumPassingTokens on a poll without revealing a vote');
+    // });
 
     it('should return 0 if the queried tokens were committed to the minority bloc', () => {
       it('voting for', async () => {
@@ -113,12 +113,12 @@ contract('PLCRVoting', (accounts) => {
         loserOptions.vote = '0';
         loserOptions.numTokens = '5';
 
-        const pollID = await utils.startPollAndCommitVote(options, plcr);
-        await utils.commitAs(pollID, loserOptions, plcr);
-        await utils.increaseTime(new BN(options.commitPeriod, 10).add(new BN('1', 10)).toNumber(10));
+        const pollID = await plcr.startPoll("50", "100", "100"); //utils.startPollAndCommitVote(options, plcr);
+        await plcr.commitVote(pollID, loserOptions, plcr);
+        await utils.increaseTime(new BN("100", 10).add(new BN('1', 10)).toNumber(10));
 
         await utils.as(options.actor, plcr.revealVote, pollID, options.vote, options.salt);
-        await utils.increaseTime(new BN(options.revealPeriod, 10).add(new BN('1', 10)).toNumber(10));
+        await utils.increaseTime(new BN("100", 10).add(new BN('1', 10)).toNumber(10));
 
         // const passingTokens = await plcr.getNumPassingTokens.call(options.actor, pollID);
         const loserPassingTokens = await plcr.getNumPassingTokens.call(loserOptions.actor, pollID);
