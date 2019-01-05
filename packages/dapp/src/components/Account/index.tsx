bf3fcbbe848daa3683c3b2b6ff078d0124d6e639
Jorge Lopez <jl5659@gmail.com>
2019-01-04 20:11:52 -0600

    New route

--- packages/dapp/src/components/Account/index.tsx
+++ packages/dapp/src/components/Account/index.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
+import { Route, Switch, RouteComponentProps } from "react-router-dom";
 
-export default class AccountRouter extends React.Component<{}, {}> {
+export default class AccountRouter extends React.Component<RouteComponentProps> {
   // constructor(props: any) {
   //   super(props);
   //   // this.state = {};
   // }
 
   public render(): JSX.Element {
-    return <>hello</>;
+    const { match } = this.props;
+
+    return (
+      <>
+        <Switch>
+          <Route path={`${match.path}/signin`} component={() => <>cool</>} />
+        </Switch>
+      </>
+    );
   }
 }
