f403d1d9ef5eac5203ac31785561ea810b223a6b
Toby Fox <tobyfox@gmail.com>
2019-01-30 16:58:36 -0500

    Remove unneeded stuff from AccountEmailAuth story

--- packages/components/src/Account/Auth/EthAuth.stories.tsx
+++ packages/components/src/Account/Auth/EthAuth.stories.tsx
@@ -1,6 +1,5 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
-import apolloStorybookDecorator from "apollo-storybook-react";
 import { Civil } from "@joincivil/core";
 import { AccountEthAuth } from "../";
 
@@ -11,56 +10,7 @@ try {
   civil = undefined;
 }
 
-const typeDefs = `
-  input UserSignatureInput {
-    message: String!
-    messageHash: String!
-    signature: String!
-    signer: String!
-    r: String!
-    s: String!
-    v: String!
-  }
-
-  type User {
-    uid: String
-    email: String
-    ethAddress: String
-  }
-
-  type Query {
-    currentUser: User
-  }
-
-  type Mutation {
-    userSetEthAddress(
-      input: UserSignatureInput!
-    ): String
-  }
-
-  schema {
-    query: Query
-    mutation: Mutation
-  }
-`;
-
-const mocks = {
-  Mutation: () => {
-    return {
-      userSetEthAddress: (input: any) => {
-        return "ok";
-      },
-    };
-  },
-};
-
 storiesOf("Account/ETH", module)
-  .addDecorator(
-    apolloStorybookDecorator({
-      typeDefs,
-      mocks,
-    }),
-  )
   .add("AccountEthAuth", () => {
     return (
       <AccountEthAuth
