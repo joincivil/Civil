060246765d412eeb1591034b7475c242c88d5613
Jon Ferrer <jon@joincivil.com>
2019-02-01 12:34:52 -0500

    Add support for Success / Error messaging to Email Signup component

--- packages/components/src/EmailSignup/EmailStyledComponents.tsx
+++ packages/components/src/EmailSignup/EmailStyledComponents.tsx
@@ -3,18 +3,12 @@ import styled, { StyledComponentClass } from "styled-components";
 
 import { colors, fonts } from "../styleConstants";
 
-export const StyledEmailSignupContainer = styled.div`
-  border: 1px solid ${colors.accent.CIVIL_GRAY_4};
-  font-family: ${fonts.SANS_SERIF};
-  box-shadow: inset 0 5px 0 0 ${colors.primary.BLACK};
-  padding: 38px 39px 24px;
-`;
-
 export const StyledEmailSignupTitle = styled.div`
   color: ${colors.primary.BLACK};
   font-size: 24px;
   font-weight: 900;
   line-height: 29px;
+  margin: 0 0 6px;
 `;
 
 export const StyledEmailSignupCopy = styled.p`
@@ -22,4 +16,23 @@ export const StyledEmailSignupCopy = styled.p`
   font-size: 14px;
   letter-spacing: -0.09px;
   line-height: 20px;
+`;
+
+export const StyledEmailSignupContainer = styled.div`
+  border: 1px solid ${colors.accent.CIVIL_GRAY_4};
+  font-family: ${fonts.SANS_SERIF};
+  box-shadow: inset 0 5px 0 0 ${colors.primary.BLACK};
+  padding: 38px 39px 24px;
+`;
+
+export const StyledEmailSignupSuccessContainer = styled(StyledEmailSignupContainer)`
+  padding-top: 92px;
+  text-align: center;
+
+  & > ${StyledEmailSignupCopy}:first-of-type {
+    font-size: 18px;
+    letter-spacing: normal;
+    line-height: 33px;
+    margin: 0 0 133px;
+  }
 `;
--- packages/components/src/icons/GreenCheckMark.tsx
+++ packages/components/src/icons/GreenCheckMark.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { colors } from "../styleConstants";
 
-export const GreenCheckMark: React.SFC = props => {
+export interface GreenCheckMark {
+  height?: number;
+  width?: number;
+}
+
+export const GreenCheckMark: React.SFC<GreenCheckMark> = props => {
+  const height = (props.height || 26).toString();
+  const width = (props.width || 26).toString();
   return (
-    <svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
+    <svg width={width} height={height} viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
       <circle fill={colors.accent.CIVIL_TEAL} cx="12" cy="12" r="12" />
       <path
         d="m9.464 17.092-3.466-3.415-1.156 1.138 4.622 4.553 9.904-9.756-1.155-1.138z"
