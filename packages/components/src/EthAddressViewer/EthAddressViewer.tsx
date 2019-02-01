b0b9df90a259d414bbf5cbb22b441ed7d3df927b
Jon Ferrer <jon@joincivil.com>
2019-01-31 20:19:23 -0500

    - Add actual component to `components` - Add `etherscan` helper

---
+++ packages/components/src/EthAddressViewer/EthAddressViewer.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+import { colors } from "../styleConstants";
+import { Button, InvertedButton, buttonSizes } from "../Button";
+import { NorthEastArrow } from "../icons";
+
+import {
+  StyledEthAdressViewer,
+  StyledDisplayName,
+  StyledEthAdressContainer,
+  StyledEthAdress,
+} from "./StyledEthAddressViewer";
+
+export interface EthAddressViewerProps {
+  address: string;
+  displayName: string;
+  etherscanBaseURL?: string;
+}
+
+export const EthAddressViewer: React.SFC<EthAddressViewerProps> = props => {
+  const { address, displayName, etherscanBaseURL } = props;
+
+  const copyToClipBoard = () => {
+    const textArea = document.createElement("textarea");
+    textArea.innerText = props.address.replace(/ /g, "")!;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+  };
+
+  const etherscanURL = etherscanBaseURL && `${etherscanBaseURL}/address/${address.replace(/ /g, "")}`;
+
+  return (
+    <StyledEthAdressViewer>
+      <StyledDisplayName>{displayName}</StyledDisplayName>
+      <StyledEthAdressContainer>
+        <StyledEthAdress>{props.address}</StyledEthAdress>
+        <Button
+          size={buttonSizes.SMALL}
+          onClick={() => {
+            copyToClipBoard();
+          }}
+        >
+          Copy
+        </Button>
+        {!!etherscanURL && (
+          <InvertedButton size={buttonSizes.SMALL} href={etherscanURL} target="_blank">
+            Etherscan <NorthEastArrow color={colors.accent.CIVIL_BLUE} />
+          </InvertedButton>
+        )}
+      </StyledEthAdressContainer>
+    </StyledEthAdressViewer>
+  );
+};
