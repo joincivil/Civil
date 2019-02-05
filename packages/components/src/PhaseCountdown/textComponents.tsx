dce40069631deb5f606205a291e758e73c7092c9
Jon Ferrer <jon@joincivil.com>
2019-02-05 14:16:12 -0500

    Add text components for various Appeal phase countdowns

--- packages/components/src/PhaseCountdown/textComponents.tsx
+++ packages/components/src/PhaseCountdown/textComponents.tsx
@@ -2,18 +2,24 @@ import * as React from "react";
 
 export const InApplicationPhaseLabelText: React.SFC = props => <>Awaiting Approval</>;
 
-export const ChallengeCommitVotePhaseLabelText: React.SFC = props => <>Under Challenge &gt; Commiting Votes</>;
+export const ChallengeCommitVotePhaseLabelText: React.SFC = props => <>Commit Vote</>;
 
-export const ChallengeRevealVotePhaseLabelText: React.SFC = props => <>Under Challenge &gt; Revealing Votes</>;
+export const ChallengeRevealVotePhaseLabelText: React.SFC = props => <>Confirm Vote</>;
 
-export const ChallengeAwaitingAppealRequestPhaseLabelText: React.SFC = props => (
-  <>Under Challenge &gt; Awaiting Appeal Request</>
-);
+export const ChallengeAwaitingAppealRequestPhaseLabelText: React.SFC = props => <>Awaiting Appeal Request</>;
+
+export const ChallengeAwaitingAppealJudgementPhaseLabelText: React.SFC = props => <>Awaiting Appeal Decision</>;
+
+export const ChallengeAwaitingAppealChallengePhaseLabelText: React.SFC = props => <>Awaiting Appeal Challenge</>;
 
 export const InApplicationFlavorText: React.SFC = props => <>until approval</>;
 
-export const ChallengeCommitVoteFlavorText: React.SFC = props => <>to submit votes</>;
+export const ChallengeCommitVoteFlavorText: React.SFC = props => <>to commit votes</>;
 
-export const ChallengeRevealVoteFlavorText: React.SFC = props => <>to reveal votes</>;
+export const ChallengeRevealVoteFlavorText: React.SFC = props => <>to confirm votes</>;
 
 export const ChallengeAwaitingAppealRequestFlavorText: React.SFC = props => <>to request an appeal</>;
+
+export const ChallengeAwaitingAppealJudgementFlavorText: React.SFC = props => <>until Council's decision</>;
+
+export const ChallengeAwaitingAppealChallengeFlavorText: React.SFC = props => <>to challenge the granted appeal</>;
