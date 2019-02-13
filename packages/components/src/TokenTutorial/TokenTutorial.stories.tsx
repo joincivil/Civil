058398bbb7403dff4b72e73ecc8716ac1d389422
Jorge Lopez <jl5659@gmail.com>
2019-02-13 10:58:15 -0600

    Trying to fix this storybook

--- packages/components/src/TokenTutorial/TokenTutorial.stories.tsx
+++ packages/components/src/TokenTutorial/TokenTutorial.stories.tsx
@@ -3,11 +3,50 @@ import * as React from "react";
 import { TokenTutorial } from "./TokenTutorial";
 import { TokenTutorialLanding } from "./TokenTutorialLanding";
 
+import apolloStorybookDecorator from "apollo-storybook-react";
+
 const onClickFunc = () => {
   console.log("clicked!");
 };
 
+const typeDefs = `
+  type User {
+    uid: String
+    email: String
+    ethAddress: String
+  }
+
+  type Query {
+    currentUser: User
+  }
+
+  schema {
+    query: Query
+  }
+`;
+
+const mocks = {
+  Query: () => {
+    return {
+      currentUser: () => {
+        return {
+          uid: "abcd",
+          email: "rad@civil.co",
+          quizPayload: {},
+          quizStatus: "",
+        };
+      },
+    };
+  },
+};
+
 storiesOf("Token Tutorial", module)
+  .addDecorator(
+    apolloStorybookDecorator({
+      typeDefs,
+      mocks,
+    }),
+  )
   .add("Token Tutorial", () => {
     return <TokenTutorial handleClose={onClickFunc} />;
   })
