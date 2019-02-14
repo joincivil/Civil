eb36ce739d783de0892b7016f88962874f6fd4f4
Jorge Lopez <jl5659@gmail.com>
2019-02-14 13:39:16 -0600

    Moved LoadUser

--- packages/components/src/Account/LoadUser.tsx
+++ packages/components/src/Account/LoadUser.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { getApolloSession, getCurrentUserQuery } from "@joincivil/utils";
-
 import { Query } from "react-apollo";
 
 export interface LoadUserChildrenProps {
