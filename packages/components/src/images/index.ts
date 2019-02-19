526cb064ee898023cf7dd3ed84c1b76165369ca5
Toby Fox <tobyfox@gmail.com>
2019-02-19 09:43:39 -0500

    Export image URLs from @joincivil/components

---
+++ packages/components/src/images/index.ts
@@ -0,0 +1,13 @@
+import * as metaMaskNetworkSwitchImgSrc from "./img-metamask-networkswitch@2x.png";
+import * as metaMaskLoginImgSrc from "./img-metamask-login@2x.png";
+import * as metaMaskFrontLargeImgSrc from "./img-metamask-large-front.png";
+import * as metaMaskConnectImgSrc from "./img-metamask-connectreq-banner@2x.png";
+import * as metaMaskSignImgSrc from "./img-metamask-sign@2x.png";
+
+export {
+  metaMaskNetworkSwitchImgSrc,
+  metaMaskLoginImgSrc,
+  metaMaskFrontLargeImgSrc,
+  metaMaskConnectImgSrc,
+  metaMaskSignImgSrc,
+};
