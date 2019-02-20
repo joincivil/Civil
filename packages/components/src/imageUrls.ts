460862bb7c5a518caeb1f517b99e954744a4c409
Toby Fox <tobyfox@gmail.com>
2019-02-19 22:41:35 -0500

    Update front MM logo image

--- packages/components/src/imageUrls.ts
+++ packages/components/src/imageUrls.ts
@@ -1,6 +1,6 @@
 import * as metaMaskNetworkSwitchImgUrl from "./images/img-metamask-networkswitch@2x.png";
 import * as metaMaskLoginImgUrl from "./images/img-metamask-login@2x.png";
-import * as metaMaskFrontLargeImgUrl from "./images/img-metamask-large-front.png";
+import * as metaMaskFrontLargeImgUrl from "./images/img-metamask-large-front@2x.png";
 import * as metaMaskConnectImgUrl from "./images/img-metamask-connectreq-banner@2x.png";
 import * as metaMaskSignImgUrl from "./images/img-metamask-sign@2x.png";
 
