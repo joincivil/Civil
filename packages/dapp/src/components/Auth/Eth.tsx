9f13eb10a81c0a99fb9b9ebb6e23d49db63f590d
Jorge Lopez <jl5659@gmail.com>
2019-02-21 16:33:08 -0600

    Wrestling with TS

--- packages/dapp/src/components/Auth/Eth.tsx
+++ packages/dapp/src/components/Auth/Eth.tsx
@@ -1,57 +1,98 @@
 import * as React from "react";
-import { AuthOuterWrapper, WalletOnboardingV2 } from "@joincivil/components";
+import { AuthOuterWrapper, WalletOnboardingV2, LoadUser } from "@joincivil/components";
 import { getCivil } from "../../helpers/civilInstance";
+import { connect } from "react-redux";
 import { hasInjectedProvider } from "@joincivil/ethapi";
+import { ethereumEnable } from "@joincivil/utils";
+import { EthAddress } from "@joincivil/core";
+import { State } from "../../redux/reducers";
 
 export interface AuthEthProps {
+  userAccount?: EthAddress;
   onAuthentication(): void;
+}
+
+export interface AuthEthState {
+  metamaskEnabled: boolean;
+}
+
+export interface AuthEthReduxProps {
+  userAccount: EthAddress;
 }
 
 // TODO(jorgelo): Allow user to add their wallet ONLY after they are signed in .
 
-export const AuthEth: React.SFC<AuthEthProps> = ({ onAuthentication }) => {
-  // TODO(jorgelo): Maybe pass civil into here.
+export class AuthEth extends React.Component<AuthEthProps, AuthEthState> {
+  constructor(props: AuthEthProps) {
+    super(props);
+    this.state = {
+      metamaskEnabled: false,
+    };
+  }
+  public async componentDidMount(): Promise<void> {
+    this.setState({ metamaskEnabled: !!(await ethereumEnable()) });
+  }
 
-  const civil = getCivil();
+  public render(): JSX.Element {
+    // TODO(jorgelo): Maybe pass civil into here.
+    const civil = getCivil();
 
-  const metamaskEnabled = true; // For now let's assume metamasks enabled. How does this differ from hasInjectedProvider?
-  const enable = () => console.log("Enable");
+    const { onAuthentication, userAccount } = this.props;
+    const { metamaskEnabled } = this.state;
 
-  const account = "0xabc1230000000000000000000000000000abc123"; // What is account? Eth address? Do we need this?
+    const metamaskWalletAddress = userAccount || undefined;
+    return (
+      <AuthOuterWrapper>
+        <LoadUser>
+          {({ loading, user }) => {
+            if (loading) {
+              return null;
+            }
 
-  // const wrongNetwork = this.props.civil && !!this.props.requiredNetwork && this.props.currentNetwork !== this.props.requiredNetwork
-  const wrongNetwork = false; // Can you have a wrong network? This is just for signing.
+            const profileWalletAddress = user && user.ethAddress;
 
-  const requiredNetworkNiceName = undefined;
-  // const requiredNetworkNiceName = this.props.requiredNetworkNiceName || this.props.requiredNetwork
+            const wrongNetwork = false;
+            const requiredNetworkNiceName = undefined;
 
-  // TODO(jorgelo): What are these?
-  const helpUrl = "http://help.com";
-  const helpUrlBase = "/";
+            // TODO(jorgelo): What are these? (We can probably get rid of these in the future)
+            const helpUrl = "http://help.com";
+            const helpUrlBase = "/";
 
-  const profileWalletAddress = undefined;
+            // This is called when the auth is complete.
+            const onOnboardingComplete = () => console.log("Wallet auth complete");
+            const enable = () => console.log("Enable");
 
-  // How do onOnboardingComplete and onContinue differ? Do we need the continue button?
-  // () => props.onAuthentication()
-  const onContinue = () => console.log("onContinue");
+            return (
+              <WalletOnboardingV2
+                civil={civil}
+                noProvider={!hasInjectedProvider()}
+                notEnabled={!metamaskEnabled}
+                enable={enable}
+                walletLocked={civil && metamaskEnabled && !metamaskWalletAddress}
+                wrongNetwork={wrongNetwork}
+                requiredNetworkNiceName={requiredNetworkNiceName}
+                metamaskWalletAddress={metamaskWalletAddress}
+                helpUrl={helpUrl}
+                helpUrlBase={helpUrlBase}
+                profileWalletAddress={profileWalletAddress}
+                onOnboardingComplete={onOnboardingComplete}
+                onContinue={onAuthentication}
+              />
+            );
+          }}
+        </LoadUser>
+      </AuthOuterWrapper>
+    );
+  }
+}
 
-  return (
-    <AuthOuterWrapper>
-      <WalletOnboardingV2
-        civil={civil}
-        noProvider={!hasInjectedProvider()}
-        notEnabled={!metamaskEnabled}
-        enable={enable}
-        walletLocked={civil && metamaskEnabled && !account}
-        wrongNetwork={wrongNetwork}
-        requiredNetworkNiceName={requiredNetworkNiceName}
-        metamaskWalletAddress={account}
-        helpUrl={helpUrl}
-        helpUrlBase={helpUrlBase}
-        profileWalletAddress={profileWalletAddress}
-        onOnboardingComplete={onAuthentication}
-        onContinue={onContinue}
-      />
-    </AuthOuterWrapper>
-  );
+const mapStateToProps = (state: State, ownProps: AuthEthProps): AuthEthProps & AuthEthReduxProps => {
+  const { user } = state.networkDependent;
+
+  return {
+    ...ownProps,
+    userAccount: user.account.account,
+  };
 };
+
+export const AuthEthConnected = connect(mapStateToProps)(AuthEth);
