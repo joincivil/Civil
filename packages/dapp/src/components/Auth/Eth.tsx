7328fe48c0871869f62dc732c1651c615291e001
Toby Fox <tobyfox@gmail.com>
2019-02-21 20:05:35 -0500

    Update storefront WalletOnboardingV2 integration

--- packages/dapp/src/components/Auth/Eth.tsx
+++ packages/dapp/src/components/Auth/Eth.tsx
@@ -2,17 +2,11 @@ import * as React from "react";
 import { AuthOuterWrapper, WalletOnboardingV2, LoadUser } from "@joincivil/components";
 import { getCivil } from "../../helpers/civilInstance";
 import { connect, DispatchProp } from "react-redux";
-import { hasInjectedProvider } from "@joincivil/ethapi";
-import { ethereumEnable } from "@joincivil/utils";
 import { EthAddress } from "@joincivil/core";
 import { State } from "../../redux/reducers";
 
 export interface AuthEthProps {
   onAuthentication(): void;
-}
-
-export interface AuthEthState {
-  metamaskEnabled: boolean;
 }
 
 export interface AuthEthReduxProps {
@@ -21,20 +15,12 @@ export interface AuthEthReduxProps {
 
 // TODO(jorgelo): Allow user to add their wallet ONLY after they are signed in .
 
-export class AuthEth extends React.Component<AuthEthProps & AuthEthReduxProps & DispatchProp<any>, AuthEthState> {
-  constructor(props: AuthEthProps & AuthEthReduxProps & DispatchProp<any>) {
-    super(props);
-    this.state = {
-      metamaskEnabled: false,
-    };
-  }
-
+export class AuthEth extends React.Component<AuthEthProps & AuthEthReduxProps & DispatchProp<any>> {
   public render(): JSX.Element {
     // TODO(jorgelo): Maybe pass civil into here.
     const civil = getCivil();
 
     const { onAuthentication, userAccount } = this.props;
-    const { metamaskEnabled } = this.state;
     const metamaskWalletAddress = userAccount || undefined;
 
     return (
@@ -47,32 +33,12 @@ export class AuthEth extends React.Component<AuthEthProps & AuthEthReduxProps & 
 
             const profileWalletAddress = user && user.ethAddress;
 
-            const wrongNetwork = false;
-            const requiredNetworkNiceName = undefined;
-
-            // TODO(jorgelo): What are these? (We can probably get rid of these in the future)
-            const helpUrl = "http://help.com";
-            const helpUrlBase = "/";
-
-            // This is called when the auth is complete.
-            const onOnboardingComplete = () => console.log("Wallet auth complete");
-            const enable = async () => this.setState({ metamaskEnabled: !!(await ethereumEnable()) });
-
             return (
               <WalletOnboardingV2
                 civil={civil}
-                noProvider={!hasInjectedProvider()}
-                notEnabled={!metamaskEnabled}
-                enable={enable}
-                walletLocked={civil && metamaskEnabled && !metamaskWalletAddress}
-                wrongNetwork={wrongNetwork}
-                requiredNetworkNiceName={requiredNetworkNiceName}
                 metamaskWalletAddress={metamaskWalletAddress}
-                helpUrl={helpUrl}
-                helpUrlBase={helpUrlBase}
                 profileWalletAddress={profileWalletAddress}
-                onOnboardingComplete={onOnboardingComplete}
-                onContinue={onAuthentication}
+                onOnboardingComplete={onAuthentication}
               />
             );
           }}
