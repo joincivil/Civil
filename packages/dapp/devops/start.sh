17d47571ae0e94313a7fe6989c1db5fc3aa594c6
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2019-02-28 10:53:45 -0500

    support test GA when developing locally

--- packages/dapp/devops/start.sh
+++ packages/dapp/devops/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "ENVIRONMENT: $ENVIRONMENT"
-echo "GA\: $GA_ID"
+echo "GA_TRACKING_ID\: $GA_TRACKING_ID"
 envsubst '\$ENVIRONMENT:\$GA_TRACKING_ID' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
 mv  /usr/share/nginx/html/index.html.tmp  /usr/share/nginx/html/index.html
 exec nginx -g 'daemon off;'
