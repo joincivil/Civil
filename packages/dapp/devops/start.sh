19540b9edfb6991ec187db87dd5d522ca5032777
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2019-02-28 10:28:41 -0500

    fix pathname, use environment variable for GA_TRACKING_ID

--- packages/dapp/devops/start.sh
+++ packages/dapp/devops/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-echo "ENVIRONMENT is: $ENVIRONMENT"
-envsubst '\$ENVIRONMENT' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
+echo "ENVIRONMENT: $ENVIRONMENT"
+echo "GA\: $GA_ID"
+envsubst '\$ENVIRONMENT:\$GA_TRACKING_ID' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
+mv  /usr/share/nginx/html/index.html.tmp  /usr/share/nginx/html/index.html
 exec nginx -g 'daemon off;'
