d69e7c5a789eb9cd587d0b6d722f0572ea8e7dbd
Dan Kinsley <dan@mgemi.com>
2019-02-27 22:07:07 -0500

    adds ConnectedRouter

--- packages/dapp/src/index.tsx
+++ packages/dapp/src/index.tsx
@@ -1,17 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./App";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { gaMiddleware } from "./redux/analytics";
-import { createBrowserHistory } from "history";
-import { routerMiddleware } from "connected-react-router";
-import createRootReducer from "./redux/reducers";
 
-export const history = createBrowserHistory();
-
-const store = createStore(createRootReducer(history), applyMiddleware(routerMiddleware(history), thunk, gaMiddleware));
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
