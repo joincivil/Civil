6f10d4092a1be33caa96749a88839658a537cbab
Peter Ng <peter@joincivil.com>
2019-03-04 16:03:14 -0500

    Updated registry alerts twitter name

--- packages/components/src/EmailSignup/EmailSignupSuccess.tsx
+++ packages/components/src/EmailSignup/EmailSignupSuccess.tsx
@@ -19,8 +19,8 @@ export const EmailSignupSuccess: React.SFC = props => {
       <StyledEmailSignupCopy>
         <b>
           Follow us on twitter for alerts{" "}
-          <a href="https://twitter.com/CivilTCRalerts" target="_blank">
-            @CivilTCRalerts
+          <a href="https://twitter.com/civil_alerts" target="_blank">
+            @Civil_Alerts
           </a>
         </b>
       </StyledEmailSignupCopy>
