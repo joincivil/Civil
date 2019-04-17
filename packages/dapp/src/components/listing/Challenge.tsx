04853e29b7fe91d00d4bafb59defa9e1bb11356c
Jon Ferrer <jon@joincivil.com>
2019-04-17 15:57:45 -0400

    - Fix bug where Appeal Summary component would render in the My Tasks Dashboard tab for challenges that are awaiting appeal request - Fix link back to Listing Detail page from Historical Challenge page

--- packages/dapp/src/components/listing/Challenge.tsx
+++ packages/dapp/src/components/listing/Challenge.tsx
@@ -35,7 +35,7 @@ export interface ChallengePageProps {
 
 class ChallengePage extends React.Component<ChallengePageProps> {
   public render(): JSX.Element {
-    const listingAddress = this.props.match.params.listing;
+    const listingAddress = this.props.match.params.listingAddress;
     const listingURL = formatRoute(routes.LISTING, { listingAddress });
     const challengeID = new BigNumber(this.props.match.params.challengeID);
     if (!this.props.useGraphQL) {
