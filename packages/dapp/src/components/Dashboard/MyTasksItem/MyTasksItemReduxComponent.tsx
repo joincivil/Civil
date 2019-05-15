674ab9c5637b28d97e84878fffda2546af8739ee
Jon Ferrer <jon@joincivil.com>
2019-05-15 12:16:47 -0400

    Refactor `MyTasksItem` to clean up GraphQL vs Web3 components

---
+++ packages/dapp/src/components/Dashboard/MyTasksItem/MyTasksItemReduxComponent.tsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+import { connect, DispatchProp } from "react-redux";
+
+import {
+  CharterData,
+  WrappedChallengeData,
+} from "@joincivil/core";
+
+import { State } from "../../../redux/reducers";
+import { ListingWrapperWithExpiry } from "../../../redux/reducers/listings";
+import { fetchAndAddListingData } from "../../../redux/actionCreators/listings";
+import { getContent } from "../../../redux/actionCreators/newsrooms";
+import {
+  getChallenge,
+  makeGetListingAddressByChallengeID,
+  makeGetUserChallengeData,
+  makeGetUserAppealChallengeData,
+  getChallengeState,
+  getAppealChallengeState,
+} from "../../../selectors";
+import { MyTasksItemOwnProps, MyTasksItemReduxProps } from "./MyTasksItemTypes";
+import MyTasksItemComponent from "./MyTasksItemComponent";
+
+class MyTasksItemReduxWrapper extends React.Component<MyTasksItemOwnProps & MyTasksItemReduxProps & DispatchProp<any>> {
+  public async componentDidUpdate(): Promise<void> {
+    await this.ensureListingAndNewsroomData();
+  }
+
+  public async componentDidMount(): Promise<void> {
+    await this.ensureListingAndNewsroomData();
+  }
+
+  public render(): JSX.Element {
+    return <MyTasksItemComponent {...this.props} />;
+  }
+
+  private ensureListingAndNewsroomData = async (): Promise<void> => {
+    if (!this.props.listing && !this.props.listingDataRequestStatus && this.props.listingAddress) {
+      this.props.dispatch!(fetchAndAddListingData(this.props.listingAddress!));
+    }
+    if (this.props.newsroom) {
+      this.props.dispatch!(await getContent(this.props.newsroom.wrapper.data.charterHeader!));
+    }
+  };
+}
+
+const makeMapStateToProps = () => {
+  const getListingAddressByChallengeID = makeGetListingAddressByChallengeID();
+  const getUserChallengeData = makeGetUserChallengeData();
+  const getUserAppealChallengeData = makeGetUserAppealChallengeData();
+
+  const mapStateToProps = (state: State, ownProps: MyTasksItemOwnProps): MyTasksItemOwnProps & MyTasksItemReduxProps => {
+    const { newsrooms } = state;
+    const { user, content, listings, listingsFetching } = state.networkDependent;
+    const userAcct = user && user.account.account;
+
+    const listingAddress = getListingAddressByChallengeID(state, ownProps);
+    const challenge = getChallenge(state, ownProps);
+    const challengeState = getChallengeState(challenge!);
+    const userChallengeData = getUserChallengeData(state, ownProps);
+
+    const appeal = challenge && challenge.challenge.appeal;
+    const appealChallengeID = appeal && appeal.appealChallengeID && appeal.appealChallengeID.toString();
+    const appealChallenge = appeal && appeal.appealChallenge;
+
+    let appealChallengeState;
+    let appealUserChallengeData;
+    if (appealChallenge) {
+      appealChallengeState = getAppealChallengeState(appealChallenge);
+      appealUserChallengeData = getUserAppealChallengeData(state, ownProps);
+    }
+
+    const listing = (listingAddress && listings.get(listingAddress)) as ListingWrapperWithExpiry | undefined;
+    const newsroom = listingAddress ? newsrooms.get(listingAddress) : undefined;
+    let charter;
+    if (newsroom && newsroom.wrapper.data.charterHeader) {
+      charter = content.get(newsroom.wrapper.data.charterHeader.uri) as CharterData;
+    }
+    let listingDataRequestStatus;
+    if (listingAddress) {
+      listingDataRequestStatus = listingsFetching.get(listingAddress.toString());
+    }
+
+    return {
+      listingAddress,
+      challenge,
+      challengeState,
+      userChallengeData,
+      appeal,
+      appealChallengeID,
+      appealChallenge,
+      appealChallengeState,
+      appealUserChallengeData,
+      user: userAcct,
+      listingDataRequestStatus,
+      newsroom,
+      charter,
+      listing: listing && listing.listing,
+      ...ownProps,
+    };
+  };
+
+  return mapStateToProps;
+};
+
+export default connect(makeMapStateToProps)(MyTasksItemReduxWrapper);
---
+++ packages/dapp/src/components/Dashboard/MyTasksItem/MyTasksItemTypes.ts
@@ -0,0 +1,51 @@
+import {
+  ContentData,
+  ListingWrapper,
+  WrappedChallengeData,
+  AppealData,
+  AppealChallengeData,
+  UserChallengeData,
+  CharterData,
+  EthAddress,
+  EthContentHeader,
+} from "@joincivil/core";
+import { NewsroomState } from "@joincivil/newsroom-signup";
+
+export interface MyTasksItemOwnProps {
+  challengeID?: string;
+  queryUserChallengeData?: UserChallengeData;
+  queryUserAppealChallengeData?: UserChallengeData;
+  showClaimRewardsTab?(): void;
+  showRescueTokensTab?(): void;
+}
+
+export interface MyTasksItemWrapperReduxProps {
+  useGraphQL?: boolean;
+  userAcct: EthAddress;
+  content: Map<string, ContentData>;
+  getCharterContent(charterHeader: EthContentHeader): Promise<void>;
+}
+
+export interface MyTasksItemReduxProps {
+  challenge?: WrappedChallengeData;
+  challengeState?: any;
+  userChallengeData?: UserChallengeData;
+  appeal?: AppealData;
+  appealChallengeID?: string;
+  appealChallenge?: AppealChallengeData;
+  appealChallengeState?: any;
+  appealUserChallengeData?: UserChallengeData;
+  user?: EthAddress;
+  newsroom?: NewsroomState;
+  charter?: CharterData;
+  listingAddress?: string;
+  listing?: ListingWrapper;
+  listingDataRequestStatus?: any;
+}
+
+export interface ViewDetailURLProps {
+  listingDetailURL: string;
+  viewDetailURL: string;
+}
+
+export type MyTasksItemSubComponentProps = MyTasksItemOwnProps & ViewDetailURLProps & MyTasksItemReduxProps;
