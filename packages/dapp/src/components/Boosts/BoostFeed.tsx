9ae3cfe78c4adfd4fd73b6cecbe8adcfb0b9a031
sruddy <sarah.ruddy@gmail.com>
2019-06-25 12:39:14 -0400

    Boost page styles (#1284)
    * feed page styles
    
    * converter styles
    
    * tests

--- packages/dapp/src/components/Boosts/BoostFeed.tsx
+++ packages/dapp/src/components/Boosts/BoostFeed.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { BoostHeaderWrapper, BoostHeader, BoostWrapper, BoostIntro, ComingSoonText } from "./BoostStyledComponents";
+import {
+  BoostHeaderWrapper,
+  BoostHeader,
+  BoostWrapper,
+  BoostIntro,
+  BoostLearnMore,
+  ComingSoonText,
+} from "./BoostStyledComponents";
 import { Helmet } from "react-helmet";
 import ScrollToTopOnMount from "../utility/ScrollToTop";
 import { BoostFeed } from "@joincivil/civil-sdk";
@@ -14,18 +21,17 @@ class BoostFeedPage extends React.Component {
         <FeatureFlag feature={"boosts-mvp"} replacement={<ComingSoonText />}>
           <BoostHeaderWrapper>
             <BoostHeader>
-              <h1>Civil Boost</h1>
-            </BoostHeader>
-          </BoostHeaderWrapper>
-          <BoostWrapper>
-            <BoostIntro>
-              <p>
+              <h1>Boosts: support the work that journalists do.</h1>
+              <BoostIntro>
                 Newsrooms around the world need your help to fund and start new projects. These Newsrooms are setting up
                 Boosts to help in get the word out with what they want to do and let their supporters and fans, like
                 you, help them do it. Support these newsrooms by funding their Boosts to help hit their goals. Good
                 reporting costs money, and the Civil community is making it happen.
-              </p>
-            </BoostIntro>
+                <BoostLearnMore>Learn More &gt;</BoostLearnMore>
+              </BoostIntro>
+            </BoostHeader>
+          </BoostHeaderWrapper>
+          <BoostWrapper>
             <BoostFeed />
           </BoostWrapper>
         </FeatureFlag>
