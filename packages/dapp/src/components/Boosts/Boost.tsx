92cf5d68383d4852a8645e09a88027fe75f9df22
sruddy <sarah.ruddy@gmail.com>
2019-07-18 12:30:28 -0400

    readded feature flag

--- packages/dapp/src/components/Boosts/Boost.tsx
+++ packages/dapp/src/components/Boosts/Boost.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { State } from "../../redux/reducers";
-import { BoostWrapper } from "./BoostStyledComponents";
+import { BoostWrapper, ComingSoonText } from "./BoostStyledComponents";
 import { Helmet } from "react-helmet";
 import ScrollToTopOnMount from "../utility/ScrollToTop";
 import { Boost } from "@joincivil/civil-sdk";
+import { FeatureFlag } from "@joincivil/components";
 
 export interface BoostPageProps {
   match: any;
@@ -20,15 +21,17 @@ class BoostPage extends React.Component<BoostPageProps> {
       <>
         <Helmet title={(this.props.editMode ? "Edit " : "") + "Civil Boost - The Civil Registry"} />
         <ScrollToTopOnMount />
-        <BoostWrapper>
-          <Boost
-            open={true}
-            boostId={this.props.boostId}
-            editMode={this.props.editMode}
-            history={this.props.history}
-            payment={this.props.payment}
-          />
-        </BoostWrapper>
+        <FeatureFlag feature={"boosts-mvp"} replacement={<ComingSoonText />}>
+          <BoostWrapper>
+            <Boost
+              open={true}
+              boostId={this.props.boostId}
+              editMode={this.props.editMode}
+              history={this.props.history}
+              payment={this.props.payment}
+            />
+          </BoostWrapper>
+        </FeatureFlag>
       </>
     );
   }
