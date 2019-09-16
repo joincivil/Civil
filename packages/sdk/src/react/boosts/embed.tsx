90f4c6ce27a28661e4871ac887607d35ac07938f
Toby Fox <tobyfox@gmail.com>
2019-09-16 07:31:27 -0400

    feat: wip sdk boost embed

--- packages/sdk/src/react/boosts/embed.tsx
+++ packages/sdk/src/react/boosts/embed.tsx
@@ -4,26 +4,32 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import { getApolloClient } from "@joincivil/utils";
-// import { Civil, makeEthersProvider, EthersProviderResult } from "@joincivil/core";
-import { Civil } from "@joincivil/core";
+import { Civil, makeEthersProvider, EthersProviderResult } from "@joincivil/core";
 import { CivilContext, buildCivilContext } from "@joincivil/components";
 import { Boost } from "./Boost";
 
 const apolloClient = getApolloClient();
 
-// const { provider }: EthersProviderResult = makeEthersProvider("1");
-const civil = new Civil();
+const { provider }: EthersProviderResult = makeEthersProvider("1");
+// @ts-ignore: @WIP/tobek Not sure what the issue is and if this needs fixing.
+const civil = new Civil({ web3Provider: provider });
 // @ts-ignore: Getting "Argument of type ... is not assignable to parameter of type ... Types have separate declarations of private property `ethApi`" because `buildCivilContext` is getting `Civil` from its copy of `@joincivil/core` and we're passing in from our copy. In practice, these `ethApi`s will be identical or matching interface.
 const civilContext = buildCivilContext(civil);
 
+const boostId = new URLSearchParams(window.location.search).get("boost");
+
 function init(): void {
   ReactDOM.render(
-    // `apolloClient as any` because of bug with types e.g. https://github.com/awslabs/aws-mobile-appsync-sdk-js/issues/166
-    <ApolloProvider client={apolloClient as any}>
-      <CivilContext.Provider value={civilContext}>
-        <Boost boostId={"1bdf971f-54e9-472d-a975-e175c5fccaa1"} open={true} />
-      </CivilContext.Provider>
-    </ApolloProvider>,
+    boostId ? (
+      // @ts-ignore: bugs with apollo types e.g. https://github.com/awslabs/aws-mobile-appsync-sdk-js/issues/166
+      <ApolloProvider client={apolloClient}>
+        <CivilContext.Provider value={civilContext}>
+          <Boost boostId={boostId} open={true} />
+        </CivilContext.Provider>
+      </ApolloProvider>
+    ) : (
+      <p>Missing Boost ID.</p>
+    ),
     document.getElementById("civil-boost"),
   );
 }
