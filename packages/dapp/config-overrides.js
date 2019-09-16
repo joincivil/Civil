a4c8877823d1c83d2073d798725e52d7497fba3c
Toby Fox <tobyfox@gmail.com>
2019-09-16 07:12:06 -0400

    chore: boost embed entry point WIP
    This isn't working: both main and boost bundle are included in main
    entry point, and no bundles are included in boost entry point. Otherwise
    it seems... basically there.

--- packages/dapp/config-overrides.js
+++ packages/dapp/config-overrides.js
@@ -1,8 +1,59 @@
 /** Selectively update react-create-app's react-scripts webpack config without ejecting the app. */
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = function override(config, env) {
+function addBoostEmbed(config, env) {
+  const isEnvDevelopment = env === "development";
+
+  let htmlWebpackPluginConfig;
+  config.plugins.forEach(plugin => {
+    if (!htmlWebpackPluginConfig && plugin.constructor.name === "HtmlWebpackPlugin" && plugin.options.filename === "index.html") {
+      htmlWebpackPluginConfig = plugin.options;
+    }
+  });
+  if (!htmlWebpackPluginConfig) {
+    throw Error("Boost Embed: Failed to find main HtmlWebpackPlugin to use as base for new bundle/entry");
+  }
+
+  const boostEmbedHtml = "/home/toby/dev/civil/Civil/packages/dapp/public/boost-embed.html";
+  const boostEmbedJs = "/home/toby/dev/civil/Civil/packages/dapp/src/boost-embed.tsx";
+
+  return {
+    ...config,
+    entry: {
+      main: config.entry,
+      "boost-embed": isEnvDevelopment
+        ? [
+            config.entry[0], // hotloader
+            boostEmbedJs
+          ]
+        : [ boostEmbedJs ],
+    },
+    output: {
+      // @TODO/WIP/tobek This might be the part that's not working and causing both the main and boost bundles to be included in main entry point and for no bundles to be included in boost entry point. Doesn't work without modifying `output` though either.
+      ...config.output,
+      filename: "static/js/[name].js",
+    },
+    plugins: [
+      ...config.plugins,
+      new HtmlWebpackPlugin(
+        Object.assign(
+          {},
+          {
+            inject: true,
+            template: boostEmbedHtml,
+            filename: "boost-embed.html",
+            chunks: ["boost-embed"],
+          },
+          htmlWebpackPluginConfig,
+        )
+      ),
+    ],
+  }
+}
+
+function addResolve(config) {
   return {
     ...config,
     resolve: {
@@ -15,4 +66,10 @@ module.exports = function override(config, env) {
       }
     }
   }
+}
+
+module.exports = function override(config, env) {
+  const newConfig = addResolve(addBoostEmbed(config, env));
+  console.log("overridden config:", newConfig);
+  return newConfig;
 }
