0f46c15f631079af6221b86da056605cb0d9089b
Toby Fox <tobyfox@gmail.com>
2019-10-08 23:51:06 -0700

    fix(dapp): set up analytics in AppProvider so boost embed can use again
    Moves analytics event setup into AppProvider, via new AnalyticsProvider,
    so that embeds can use. This was done in a clunky way in #1430 but was
    removed by refactor in #1432, but the refactor supports a simpler way to
    implement it.

--- packages/dapp/src/registry/RegistryApp.tsx
+++ packages/dapp/src/registry/RegistryApp.tsx
@@ -1,25 +1,12 @@
 import * as React from "react";
-import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import { CivilContext, ICivilContext } from "@joincivil/components";
 import { Web3AuthWrapper } from "../components/Web3AuthWrapper";
 import Main from "../components/Main";
 import Footer from "../components/footer/Footer";
-import { analyticsEvent } from "../redux/actionCreators/analytics";
 import { NavBar } from "../components/header/NavBar";
 import { CivilHelperProvider } from "../apis/CivilHelper";
 
 export const RegistrySection: React.FunctionComponent = () => {
-  const civilCtx = React.useContext<ICivilContext>(CivilContext);
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    function fireAnalyticsEvent(category: string, action: string, label: string, value: number): void {
-      dispatch!(analyticsEvent({ category, action, label, value }));
-    }
-    civilCtx.setAnalyticsEvent(fireAnalyticsEvent);
-  }, [civilCtx, dispatch]);
-
   return (
     <React.Suspense fallback={<></>}>
       <CivilHelperProvider>
