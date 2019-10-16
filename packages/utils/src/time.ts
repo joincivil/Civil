1ddbd5143c70be85f9673eac14e73a17a74e67ab
sruddy <sarah.ruddy@gmail.com>
2019-10-16 16:26:14 -0400

    fix: time abbreviations

--- packages/utils/src/time.ts
+++ packages/utils/src/time.ts
@@ -6,8 +6,6 @@ export function getTimeSince(createdAt: string): string {
 
   const years = Math.floor(timeSinceSeconds / (86400 * 365));
   timeSinceSeconds -= years * 3600 * 24;
-  const months = Math.floor(timeSinceSeconds / (86400 * 30));
-  timeSinceSeconds -= months * 3600 * 24;
   const weeks = Math.floor(timeSinceSeconds / (86400 * 7));
   timeSinceSeconds -= weeks * 3600 * 24;
   const days = Math.floor(timeSinceSeconds / 86400);
@@ -17,19 +15,15 @@ export function getTimeSince(createdAt: string): string {
   const mins = Math.floor(timeSinceSeconds / 60);
 
   if (years >= 1) {
-    timeSince = years === 1 ? "1 year ago" : years + " years ago";
-  } else if (years < 1 && months >= 1) {
-    timeSince = months === 1 ? "1 month ago" : months + " months ago";
-  } else if (months < 1 && weeks >= 1) {
-    timeSince = weeks === 1 ? "1 week ago" : weeks + " weeks ago";
+    timeSince = years + "y ago";
+  } else if (years < 1 && weeks >= 1) {
+    timeSince = weeks + "w ago";
   } else if (weeks < 1 && days >= 1) {
-    timeSince = days === 1 ? "1 day ago" : days + " days ago";
+    timeSince = days + "d ago";
   } else if (days < 1 && hours >= 1) {
-    timeSince = hours === 1 ? "1 hour ago" : hours + " hours ago";
-  } else if (hours < 1 && mins > 1) {
-    timeSince = mins + " mins ago";
+    timeSince = hours + "h ago";
   } else {
-    timeSince = "1 min ago";
+    timeSince = mins + "m ago";
   }
 
   return timeSince;
