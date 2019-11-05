026b61f3794a6eea405c4b49c43bf66240a105ca
Nick Reynolds <nick@joincivil.com>
2019-11-04 21:34:48 -0500

    feat: first pass mobile responsive dashboard
    - also fix for paramter votes showing up in 'all' when they shouldn't

--- packages/components/src/TCRUserDashboard/Dashboard.tsx
+++ packages/components/src/TCRUserDashboard/Dashboard.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
 import styled from "styled-components";
-import { colors, fonts } from "../styleConstants";
+import { colors, fonts, mediaQueries } from "../styleConstants";
 
 const StyledDashboardHeaderOuter = styled.div`
-  background-color: ${colors.accent.CIVIL_BLUE_VERY_FADED_2};
   display: flex;
   padding: 62px 0 38px;
   justify-content: center;
+  width: 400px;
+  ${mediaQueries.MOBILE}  {
+    width: 100%;
+  }
 `;
 
 const StyledDashboardHeader = styled.div`
   color: ${colors.primary.CIVIL_GRAY_1};
   font-famliy: ${fonts.SERIF};
-  width: 1200px;
 `;
 
 export const UserDashboardHeader: React.FunctionComponent = props => {
