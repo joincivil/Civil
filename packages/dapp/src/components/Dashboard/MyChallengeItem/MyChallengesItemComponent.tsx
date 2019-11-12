d34ecd5540cd7083513e6b3622b0921eb88f903a
Nick Reynolds <nick@joincivil.com>
2019-11-12 11:28:35 -0500

    refactor: improve user challenges started query

---
+++ packages/dapp/src/components/Dashboard/MyChallengeItem/MyChallengesItemComponent.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { formatRoute } from "react-router-named-routes";
+import { DashboardActivityItemTask } from "@joincivil/components";
+
+import { routes } from "../../../constants";
+import { MyChallengesItemOwnProps, MyChallengesItemReduxProps } from "./MyChallengesItemTypes";
+import MyTasksItemPhaseCountdown from "../MyTasksItemPhaseCountdown";
+import DashboardItemChallengeResults from "../ChallengeSummary";
+
+const MyChallengesItemComponent: React.FunctionComponent<MyChallengesItemOwnProps & MyChallengesItemReduxProps> = props => {
+  const { listingAddress: address, listing, newsroom, charter, challenge, userChallengeData, challengeState } = props;
+
+  if (!challengeState) {
+    return <></>;
+  }
+
+  let canUserCollect = false;
+  let canUserRescue = false;
+  let didUserCommit = false;
+  if (userChallengeData) {
+    canUserCollect = userChallengeData.canUserCollect!;
+    canUserRescue = userChallengeData.canUserRescue!;
+    didUserCommit = userChallengeData.didUserCommit!;
+  }
+  const { inCommitPhase, inRevealPhase } = challengeState;
+
+  if (listing && listing.data && newsroom) {
+    const newsroomData = newsroom.wrapper.data;
+    const listingDetailURL = formatRoute(routes.LISTING, { listingAddress: address });
+    let viewDetailURL = listingDetailURL;
+    const title = `${newsroomData.name} Challenge #${challenge.challengeID}`;
+    const logoUrl = charter && charter.logoUrl;
+
+    if (canUserCollect || canUserRescue) {
+      viewDetailURL = formatRoute(routes.CHALLENGE, { listingAddress: address, challengeID: challenge.challengeID });
+    }
+
+    const viewProps = {
+      title,
+      logoUrl,
+      viewDetailURL,
+    };
+
+    if (canUserCollect || canUserRescue || didUserCommit) {
+      return (
+        <DashboardActivityItemTask {...viewProps}>
+          <MyTasksItemPhaseCountdown {...props} />
+          {!inCommitPhase && !inRevealPhase && (
+            <DashboardItemChallengeResults
+              listingDetailURL={listingDetailURL}
+              viewDetailURL={viewDetailURL}
+              {...props}
+            />
+          )}
+        </DashboardActivityItemTask>
+      );
+    }
+  }
+
+  return <></>;
+};
+
+export default MyChallengesItemComponent;
