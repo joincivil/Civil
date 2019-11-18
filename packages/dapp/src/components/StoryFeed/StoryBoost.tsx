f85a43a41feb48138c1c3d357e69f35715d9ac38
sruddy <sarah.ruddy@gmail.com>
2019-11-18 17:52:30 -0500

    fix: share copy not working (#1545)

--- packages/dapp/src/components/StoryFeed/StoryBoost.tsx
+++ packages/dapp/src/components/StoryFeed/StoryBoost.tsx
@@ -12,7 +12,6 @@ import {
 import { StoryDetails } from "./StoryDetails";
 import { StoryNewsroomDetails } from "./StoryNewsroomDetails";
 import { StoryModal } from "./StoryModal";
-import { ShareStory, SharePanel } from "@joincivil/elements";
 import { StoryBoostData } from "./types";
 import { STORY_BOOST } from "./queries";
 
@@ -27,91 +26,65 @@ export interface StoryBoostProps {
   handlePaymentSuccess(): void;
 }
 
-export interface StoryBoostState {
-  isStoryModalOpen: boolean;
-  isStoryNewsroomModalOpen: boolean;
-  isPaymentsModalOpen: boolean;
-  isShareModalOpen: boolean;
-}
+export const StoryBoost: React.FunctionComponent<StoryBoostProps> = props => {
+  const id = props.postId;
+  const isStoryModalOpen = props.payment || props.newsroom ? false : true;
+  const isStoryNewsroomModalOpen = props.newsroom || false;
+  const isPaymentsModalOpen = props.payment || false;
 
-export class StoryBoost extends React.Component<StoryBoostProps, StoryBoostState> {
-  public constructor(props: any) {
-    super(props);
-    this.state = {
-      isStoryModalOpen: this.props.payment || this.props.newsroom ? false : true,
-      isStoryNewsroomModalOpen: this.props.newsroom || false,
-      isPaymentsModalOpen: this.props.payment || false,
-      isShareModalOpen: false,
-    };
-  }
+  return (
+    <Query query={STORY_BOOST} variables={{ id }}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <LoadingMessage>Loading Boost</LoadingMessage>;
+        } else if (error || !data || !data.postsGet) {
+          console.error("error loading Story Boost data. error:", error, "data:", data);
+          return "Error loading Story Boost.";
+        }
 
-  public render(): JSX.Element {
-    const id = this.props.postId;
+        const storyBoostData = data.postsGet as StoryBoostData;
+        const theme = {
+          ...DEFAULT_CHECKBOX_THEME,
+          ...DEFAULT_BUTTON_THEME,
+          renderContext: RENDER_CONTEXT.DAPP,
+        };
 
-    return (
-      <Query query={STORY_BOOST} variables={{ id }}>
-        {({ loading, error, data }) => {
-          if (loading) {
-            return <LoadingMessage>Loading Boost</LoadingMessage>;
-          } else if (error || !data || !data.postsGet) {
-            console.error("error loading Story Boost data. error:", error, "data:", data);
-            return "Error loading Story Boost.";
-          }
-
-          const storyBoostData = data.postsGet as StoryBoostData;
-          const theme = {
-            ...DEFAULT_CHECKBOX_THEME,
-            ...DEFAULT_BUTTON_THEME,
-            renderContext: RENDER_CONTEXT.DAPP,
-          };
-
-          return (
-            <>
-              <StoryModal open={this.state.isStoryModalOpen} handleClose={this.props.closeStoryBoost}>
-                <StoryDetails
-                  activeChallenge={false}
-                  createdAt={storyBoostData.createdAt}
+        return (
+          <>
+            <StoryModal open={isStoryModalOpen} handleClose={props.closeStoryBoost}>
+              <StoryDetails
+                activeChallenge={false}
+                createdAt={storyBoostData.createdAt}
+                newsroomName={storyBoostData.channel.newsroom.charter.name}
+                title={storyBoostData.openGraphData.title}
+                url={storyBoostData.openGraphData.url}
+                openGraphData={storyBoostData.openGraphData}
+                displayedContributors={storyBoostData.groupedSanitizedPayments}
+                sortedContributors={storyBoostData.groupedSanitizedPayments}
+                totalContributors={
+                  storyBoostData.groupedSanitizedPayments ? storyBoostData.groupedSanitizedPayments.length : 0
+                }
+                handlePayments={props.openPayments}
+                handleOpenNewsroom={props.openStoryNewsroomDetails}
+              />
+            </StoryModal>
+            <StoryModal open={isStoryNewsroomModalOpen} handleClose={props.closeStoryBoost}>
+              <StoryNewsroomDetails activeChallenge={false} newsroom={storyBoostData.channel.newsroom} />
+            </StoryModal>
+            <ThemeProvider theme={theme}>
+              <PaymentsModal open={isPaymentsModalOpen} handleClose={props.closeStoryBoost}>
+                <Payments
+                  postId={props.postId}
                   newsroomName={storyBoostData.channel.newsroom.charter.name}
-                  openGraphData={storyBoostData.openGraphData}
-                  displayedContributors={storyBoostData.groupedSanitizedPayments}
-                  sortedContributors={storyBoostData.groupedSanitizedPayments}
-                  totalContributors={
-                    storyBoostData.groupedSanitizedPayments ? storyBoostData.groupedSanitizedPayments.length : 0
-                  }
-                  handleShare={this.openShare}
-                  handlePayments={this.props.openPayments}
-                  handleOpenNewsroom={this.props.openStoryNewsroomDetails}
+                  paymentAddress={storyBoostData.channel.newsroom.multisigAddress}
+                  isStripeConnected={storyBoostData.channel.isStripeConnected}
+                  handleClose={props.handlePaymentSuccess}
                 />
-              </StoryModal>
-              <StoryModal open={this.state.isStoryNewsroomModalOpen} handleClose={this.props.closeStoryBoost}>
-                <StoryNewsroomDetails activeChallenge={false} newsroom={storyBoostData.channel.newsroom} />
-              </StoryModal>
-              <ThemeProvider theme={theme}>
-                <PaymentsModal open={this.state.isPaymentsModalOpen} handleClose={this.props.closeStoryBoost}>
-                  <Payments
-                    postId={this.props.postId}
-                    newsroomName={storyBoostData.channel.newsroom.charter.name}
-                    paymentAddress={storyBoostData.channel.newsroom.multisigAddress}
-                    isStripeConnected={storyBoostData.channel.isStripeConnected}
-                    handleClose={this.props.handlePaymentSuccess}
-                  />
-                </PaymentsModal>
-              </ThemeProvider>
-              <SharePanel open={this.state.isShareModalOpen} handleClose={this.handleCloseShare}>
-                <ShareStory title={storyBoostData.openGraphData.title} url={storyBoostData.openGraphData.url} />
-              </SharePanel>
-            </>
-          );
-        }}
-      </Query>
-    );
-  }
-
-  private openShare = () => {
-    this.setState({ isShareModalOpen: true });
-  };
-
-  private handleCloseShare = () => {
-    this.setState({ isShareModalOpen: false });
-  };
-}
+              </PaymentsModal>
+            </ThemeProvider>
+          </>
+        );
+      }}
+    </Query>
+  );
+};
