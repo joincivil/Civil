41889c62149d9592dbe46bdf88b3d5dc9e591bdf
Toby Fox <tobyfox@gmail.com>
2019-12-10 10:52:28 -0800

    feat: prevent uploading large charter/roster images [CIVIL-1325]

--- packages/components/src/input/ImageFileToDataUri.tsx
+++ packages/components/src/input/ImageFileToDataUri.tsx
@@ -7,11 +7,20 @@ import Dropzone from "react-dropzone";
 
 export interface ImageFileToDataUriState {
   modalOpen: boolean;
+  error?: string;
 }
 
 export interface ImageFileToDataUriProps {
   buttonText?: string;
+  maxBytes?: number;
+  maxSizeText?: string;
+  info?: string;
   onChange(dataUri: string): void;
+}
+export interface ImageFileToDataUriDefaultProps {
+  maxBytes: number;
+  maxSizeText: string;
+  info: string;
 }
 
 const DropArea = styled.div`
@@ -42,8 +51,21 @@ const ButtonContainer = styled.div`
   margin-top: 25px;
 `;
 
-export class ImageFileToDataUri extends React.Component<ImageFileToDataUriProps, ImageFileToDataUriState> {
-  constructor(props: ImageFileToDataUriProps) {
+const Error = styled.p`
+  color: ${colors.accent.CIVIL_RED};
+`;
+
+export class ImageFileToDataUri extends React.Component<
+  ImageFileToDataUriProps & ImageFileToDataUriDefaultProps,
+  ImageFileToDataUriState
+> {
+  public static defaultProps: ImageFileToDataUriDefaultProps = {
+    maxBytes: 256 * 1024,
+    maxSizeText: "250KB",
+    info: "Recommended dimensions 260x260px, maximum file size 250KB. Image will be displayed constrained to a square.",
+  };
+
+  constructor(props: ImageFileToDataUriProps & ImageFileToDataUriDefaultProps) {
     super(props);
     this.state = {
       modalOpen: false,
@@ -55,6 +77,7 @@ export class ImageFileToDataUri extends React.Component<ImageFileToDataUriProps,
     }
     return (
       <Modal>
+        <p>{this.props.info}</p>
         <Dropzone onDrop={this.createDataUrlFromFile}>
           {({ getRootProps, getInputProps }: any) => (
             <section>
@@ -65,6 +88,7 @@ export class ImageFileToDataUri extends React.Component<ImageFileToDataUriProps,
             </section>
           )}
         </Dropzone>
+        {this.state.error && <Error>{this.state.error}</Error>}
         <ButtonContainer>
           <BorderlessButton size={buttonSizes.SMALL} onClick={() => this.setState({ modalOpen: false })}>
             Close
@@ -89,8 +113,19 @@ export class ImageFileToDataUri extends React.Component<ImageFileToDataUriProps,
   private createDataUrlFromFile = (acceptedFiles: any) => {
     const reader = new FileReader();
     reader.onload = (event: any) => {
-      this.props.onChange(event.target!.result);
-      this.setState({ modalOpen: false });
+      const dataUri = event.target!.result;
+      // Image data URI lengths are 1/3 longer than number of bytes in image file, don't know exactly why but tested for jpgs gifs and pngs of various sizes and it's spot on so divide by 4/3 to get bytes.
+      const fileSize = dataUri && dataUri.length / (4 / 3);
+      if (fileSize && fileSize > this.props.maxBytes) {
+        this.setState({
+          error: `Uploaded image is ${Math.round(fileSize / 1024)}KB, which is too large. Max size: ${
+            this.props.maxSizeText
+          }`,
+        });
+        return;
+      }
+      this.props.onChange(dataUri);
+      this.setState({ modalOpen: false, error: undefined });
     };
     reader.readAsDataURL(acceptedFiles[0]);
   };
