445cfd2441b2df341d2628b2b6c2510709a08b41
Toby Fox <tobyfox@gmail.com>
2020-02-05 17:08:12 -0800

    fix: don't break if no transactions duh

--- packages/dapp/src/components/Dashboard/Account/AccountTransactions.tsx
+++ packages/dapp/src/components/Dashboard/Account/AccountTransactions.tsx
@@ -94,7 +94,7 @@ interface TransactionData {
 interface PaymentHistoryData {
   currentUser: {
     userChannel: {
-      paymentsMadeByChannel: TransactionData[];
+      paymentsMadeByChannel?: TransactionData[];
     };
   };
 }
@@ -124,6 +124,14 @@ export const AccountTransactions: React.FunctionComponent = () => {
             } else if (error || !data) {
               console.error("error querying currentUser for paymentsMadeByChannel:", error || "no data returned");
               return <ErrorLoadingData />;
+            } else if (!data.currentUser.userChannel.paymentsMadeByChannel) {
+              return (
+                <p>
+                  You have made no transactions yet. Head on over to the{" "}
+                  <Link to={formatRoute(routes.STORY_FEED)}>Civil story feed</Link> to find great work that deserves
+                  your support!
+                </p>
+              );
             }
 
             const transactions = data.currentUser.userChannel.paymentsMadeByChannel
